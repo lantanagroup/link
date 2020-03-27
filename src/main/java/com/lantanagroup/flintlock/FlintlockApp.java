665de2055284d25692a5cef88b4ff9a1f3589e4f
Rick Geimer <rick.geimer@lantanagroup.com>
2020-03-27 15:50:26 -0400

    Initial commit of SpringBoot project files.

---
+++ src/main/java/com/lantanagroup/flintlock/FlintlockApp.java
@@ -0,0 +1,16 @@
+package com.lantanagroup.flintlock;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlintlockApp {
+	
+
+	public static void main(String[] args) {
+	    SpringApplication.run(FlintlockApp.class, args);
+	  }
+
+
+}
