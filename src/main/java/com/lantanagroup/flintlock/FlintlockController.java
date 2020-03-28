73e095afbe58cd044347aaa4da3eda84d9c3442b
Rick Geimer <rick.geimer@lantanagroup.com>
2020-03-28 16:33:20 -0400

    Added logging

--- src/main/java/com/lantanagroup/flintlock/FlintlockController.java
+++ src/main/java/com/lantanagroup/flintlock/FlintlockController.java
@@ -1,6 +1,8 @@
 package com.lantanagroup.flintlock;
 
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +15,15 @@ import ca.uhn.fhir.parser.IParser;
 
 @RestController
 public class FlintlockController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FlintlockController.class);
 
 	FhirContext ctx = FhirContext.forR4();
 	IParser xmlParser = ctx.newXmlParser();
 	IParser jsonParser = ctx.newJsonParser();
+	
+	public FlintlockController() {
+	}
 	
 	@RequestMapping("fhir")
 	public String transaction (@RequestBody String resourceStr) throws JsonProcessingException {
@@ -29,12 +36,13 @@ public class FlintlockController {
 			resource = xmlParser.parseResource(resourceStr);
 		}
 		String parsedResource = xmlParser.encodeResourceToString(resource);
-		System.out.println(parsedResource);
+		logger.info(parsedResource);
 		return parsedResource;
 	}
 	
 	@GetMapping("test")
 	public String test() {
+		logger.info("Example log from {}", FlintlockController.class.getSimpleName());
 		return "Hello";
 	}
 }
