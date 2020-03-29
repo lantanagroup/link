8da6d403d37a5407ba379f6798609c257982a48e
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2020-03-29 12:47:25 -0700

    Stubbing out remaining sections for ElectronicCaseReport class

--- src/main/java/com/lantanagroup/flintlock/ecr/ElectronicCaseReport.java
+++ src/main/java/com/lantanagroup/flintlock/ecr/ElectronicCaseReport.java
@@ -1,49 +1,105 @@
 package com.lantanagroup.flintlock.ecr;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-import org.hl7.fhir.r4.model.*;
-import org.hl7.fhir.r4.model.Composition.SectionComponent;
+import org.hl7.fhir.dstu3.model.*;
+import org.hl7.fhir.dstu3.model.Composition.SectionComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.parser.IParser;
 
 public class ElectronicCaseReport {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ElectronicCaseReport.class);
 	public static final String LOINC_CODE_SYSTEM = "http://loinc.org";
-	FhirContext ctx = FhirContext.forR4();
 	Composition ecr = new Composition();
-	SectionComponent problemSection;
+	SectionComponent historyOfPresentIllness;
+	SectionComponent reasonForVisit;
+	SectionComponent socialHistory;
+	SectionComponent problems;
+	SectionComponent medicationsAdministered;
+	SectionComponent results;
+	SectionComponent labOrder;
+	SectionComponent immunizations;
+	List<DomainResource> resources = new ArrayList();
 	
-	public ElectronicCaseReport() {
-		problemSection = addSection("Problems Section", "11450-4");
-		// TODO: add remaining required sections from http://hl7.org/fhir/uv/ecr/2018Jan/StructureDefinition-eicr-composition.html
+	public ElectronicCaseReport(Patient subject) {
+		this.ecr.setId(UUID.randomUUID().toString());
+		this.ecr.setSubject(new Reference(this.addResource(subject)));
+
+		historyOfPresentIllness = addSection("History of Present illness Narrative", "10164-2");
+		reasonForVisit = addSection("Reason for visit Narrative", "29299-5");
+		socialHistory = addSection("Social history Narrative", "29762-2");
+		problems = addSection("Problems Section", "11450-4");
+		medicationsAdministered = addSection("Medications administered Narrative", "29549-3");
+		results = addSection("Relevant diagnostic tests/laboratory data Narrative", "30954-2");
+		labOrder = addSection("Lab Order Narrative", "NNNNN-N");
+		immunizations = addSection("History of Immunization Narrative", "11369-6");
+	}
+
+	private String addResource(DomainResource resource) {
+		resource.setId(UUID.randomUUID().toString());
+		this.resources.add(resource);
+		return "urn:uuid:" + resource.getId();
 	}
 	
-	private SectionComponent addSection(String title, String loincCode) {
+	private SectionComponent addSection(String title, String code) {
 		SectionComponent section = ecr.addSection();
 		CodeableConcept cc = new CodeableConcept();
 		Coding c = cc.addCoding();
-		c.setCode(loincCode);
+		c.setCode(code);
 		c.setSystem(LOINC_CODE_SYSTEM);
 		this.ecr.addSection(section);
 		return section;
 	}
-	
-	public void setSubject(Patient p) {
-		// TODO: set Composition.subject to the Patient
+
+	public void setSocialHistoryEntries(List<Observation> observations) {
+		for (Observation observation : observations) {
+			String ref = this.addResource(observation);
+			this.socialHistory.addEntry(new Reference(ref));
+		}
 	}
 	
-	public void setProblemSectionEntries(List Condition) {
-		// TODO: add all Condition resources to the problemSection
+	public void setProblemSectionEntries(List<Condition> conditions) {
+		for (Condition condition : conditions) {
+			String ref = this.addResource(condition);
+			this.problems.addEntry(new Reference(ref));
+		}
+	}
+
+	public void setMedicationsAdministeredEntries(List<MedicationStatement> medicationStatements) {
+		for (MedicationStatement medicationStatement : medicationStatements) {
+			String ref = this.addResource(medicationStatement);
+			this.medicationsAdministered.addEntry(new Reference(ref));
+		}
+	}
+
+	public void setResultsEntries(List<DiagnosticReport> diagnosticReports) {
+		for (DiagnosticReport diagnosticReport : diagnosticReports) {
+			String ref = this.addResource(diagnosticReport);
+			this.results.addEntry(new Reference(ref));
+		}
+	}
+
+	public void setLabOrderEntries(List<ProcedureRequest> procedureRequests) {
+		for (ProcedureRequest procedureRequest : procedureRequests) {
+			String ref = this.addResource(procedureRequest);
+			this.labOrder.addEntry(new Reference(ref));
+		}
+	}
+
+	public void setImmunizationsEntries(List<Immunization> immunizations) {
+		for (Immunization immunization : immunizations) {
+			String ref = this.addResource(immunization);
+			this.immunizations.addEntry(new Reference(ref));
+		}
 	}
 	
 	// TODO: Add methods for populating entries for remaining sections.
-
 
 	public Bundle compile() {
 		Bundle doc = new Bundle();
@@ -52,6 +108,11 @@ public class ElectronicCaseReport {
 		// Composition first
 		doc.addEntry(createBundleEntry(this.ecr));
 
+		for (DomainResource resource : this.resources) {
+			doc.addEntry(createBundleEntry(resource));
+		}
+
+		// Set the total resources in the bundle
 		doc.setTotal(doc.getEntry().size());
 
 		return doc;
--- src/main/test/com/lantanagroup/flintlock/ecr/ElectronicCaseReportTests.java
+++ src/main/test/com/lantanagroup/flintlock/ecr/ElectronicCaseReportTests.java
@@ -1,18 +1,60 @@
 package com.lantanagroup.flintlock.ecr;
 
-import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.dstu3.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ElectronicCaseReportTests {
+
+    private static Patient createTestPatient(String ssn, String first, String last) {
+        Patient patient = new Patient();
+        Identifier newIdent = patient.addIdentifier();
+        newIdent.setValue(ssn);
+        newIdent.setSystem("http://hl7.org/fhir/sid/us-ssn");
+        HumanName newName = patient.addName();
+        newName.addGiven(first);
+        newName.setFamily(last);
+        return patient;
+    }
+
+    private static Observation createTestObservation(String code, String valueCode) {
+        Observation obs = new Observation();
+        CodeableConcept ccc = new CodeableConcept();
+        Coding codeCoding = ccc.addCoding();
+        codeCoding.setCode(code);
+        obs.setCode(ccc);
+        CodeableConcept vcc = new CodeableConcept();
+        Coding valueCoding = vcc.addCoding();
+        valueCoding.setCode(valueCode);
+        obs.setValue(vcc);
+        return obs;
+    }
 
     @Test
     public void testNewElectronicCaseReport() {
-        ElectronicCaseReport ecr = new ElectronicCaseReport();
+        Patient testPatient = createTestPatient("432-13-1234", "Bruce", "Wayne");
+        ElectronicCaseReport ecr = new ElectronicCaseReport(testPatient);
+
+        List<Observation> socialHistoryObservations = new ArrayList();
+        socialHistoryObservations.add(createTestObservation("1234", "4321"));
+        ecr.setSocialHistoryEntries(socialHistoryObservations);
+
         Bundle doc = ecr.compile();
 
         Assert.assertNotNull(doc);
-        Assert.assertEquals(1, doc.getTotal());
-        Assert.assertEquals(1, doc.getEntry().size());
+        Assert.assertEquals(3, doc.getTotal());
+        Assert.assertEquals(3, doc.getEntry().size());
+
+        Assert.assertEquals(ResourceType.Composition, doc.getEntry().get(0).getResource().getResourceType());
+        Assert.assertNotNull(doc.getEntry().get(0).getResource().getId());
+
+        Assert.assertEquals(ResourceType.Patient, doc.getEntry().get(1).getResource().getResourceType());
+        Assert.assertNotNull(doc.getEntry().get(1).getResource().getId());
+
+        Assert.assertEquals(ResourceType.Observation, doc.getEntry().get(2).getResource().getResourceType());
+        Assert.assertNotNull(doc.getEntry().get(2).getResource().getId());
     }
 }
