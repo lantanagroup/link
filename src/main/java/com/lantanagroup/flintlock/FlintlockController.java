30df3bc52b211949ec7864e1d58c2ff63a9c9835
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2020-04-02 11:12:55 -0700

    Adding basic support for retrieving heat-map coordinate data

--- src/main/java/com/lantanagroup/flintlock/FlintlockController.java
+++ src/main/java/com/lantanagroup/flintlock/FlintlockController.java
@@ -5,22 +5,26 @@ import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.validation.*;
 import ca.uhn.fhir.validation.schematron.SchematronBaseValidator;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
 import com.lantanagroup.flintlock.client.ValueSetQueryClient;
 import com.lantanagroup.flintlock.ecr.ElectronicCaseReport;
+import com.lantanagroup.flintlock.model.SimplePosition;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.*;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.Bundle.BundleType;
 import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
-import org.hl7.fhir.r4.model.Condition;
-import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.ValueSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,103 +32,202 @@ import java.util.Map;
 @RestController
 public class FlintlockController {
 
-    private static final Logger logger = LoggerFactory.getLogger(FlintlockController.class);
-    public String conformanceServerBase = "https://flintlock-fhir.lantanagroup.com/fhir";
-    public String clinicalDataServerBase = "http://hapi.fhir.org/baseR4";
-    FhirContext ctx = FhirContext.forR4();
-    IParser xmlParser = ctx.newXmlParser().setPrettyPrint(true);
-    IParser jsonParser = ctx.newJsonParser().setPrettyPrint(true);
-    ValueSetQueryClient vsClient;
-    IGenericClient clinicalDataClient;
-    String symptomsValueSetUrl = "http://flintlock-fhir.lantanagroup.com/fhir/ValueSet/symptoms";
-    String dxtcSnomedValueSetUrl = "https://flintlock-fhir.lantanagroup.com/fhir/ValueSet/dxtc-snomed";
-    String dxtcCoronavirusValueSetUrl = "https://flintlock-fhir.lantanagroup.com/fhir/ValueSet/dxtc-coronavirus";
+  private static final Logger logger = LoggerFactory.getLogger(FlintlockController.class);
+  public String conformanceServerBase = "https://flintlock-fhir.lantanagroup.com/fhir";
+  public String clinicalDataServerBase = "http://hapi.fhir.org/baseR4";
+  FhirContext ctx = FhirContext.forR4();
+  IParser xmlParser = ctx.newXmlParser().setPrettyPrint(true);
+  IParser jsonParser = ctx.newJsonParser().setPrettyPrint(true);
+  ValueSetQueryClient vsClient;
+  IGenericClient clinicalDataClient;
+  String symptomsValueSetUrl = "http://flintlock-fhir.lantanagroup.com/fhir/ValueSet/symptoms";
+  String dxtcSnomedValueSetUrl = "https://flintlock-fhir.lantanagroup.com/fhir/ValueSet/dxtc-snomed";
+  String dxtcCoronavirusValueSetUrl = "https://flintlock-fhir.lantanagroup.com/fhir/ValueSet/dxtc-coronavirus";
 
-    public FlintlockController() {
-        this.vsClient = new ValueSetQueryClient(conformanceServerBase, clinicalDataServerBase);
-        this.clinicalDataClient = this.ctx.newRestfulGenericClient(clinicalDataServerBase);
-        //ctx.setNarrativeGenerator(new DefaultThymeleafNarrativeGenerator());
+  public FlintlockController() {
+    this.vsClient = new ValueSetQueryClient(conformanceServerBase, clinicalDataServerBase);
+    this.clinicalDataClient = this.ctx.newRestfulGenericClient(clinicalDataServerBase);
+    //ctx.setNarrativeGenerator(new DefaultThymeleafNarrativeGenerator());
+  }
+
+  public ValidationResult validate(IBaseResource resource) {
+    ValidationResult result = null;
+    FhirValidator validator = ctx.newValidator();
+    IValidatorModule module1 = new SchemaBaseValidator(ctx);
+    validator.registerValidatorModule(module1);
+    IValidatorModule module2 = new SchematronBaseValidator(ctx);
+    validator.registerValidatorModule(module2);
+    result = validator.validateWithResult(resource);
+    for (SingleValidationMessage next : result.getMessages()) {
+      System.out.println(next.getLocationString() + " " + next.getMessage());
+    }
+    return result;
+  }
+
+
+  @GetMapping(value = "patients", produces = "application/fhir+xml")
+  public String patients() {
+    ValueSet vs = vsClient.getValueSet(dxtcSnomedValueSetUrl);
+    logger.info("Retrieved value set", vs.getUrl());
+    List<Condition> resultList = vsClient.conditionCodeQuery(vs);
+    Map<String, Patient> patientRefs = getUniquePatientReferences(resultList);
+    Bundle b = new Bundle();
+    b.setType(BundleType.COLLECTION);
+    for (String key : patientRefs.keySet()) {
+      Patient p = patientRefs.get(key);
+      BundleEntryComponent entry = b.addEntry();
+      entry.setFullUrl(clinicalDataServerBase + "/" + key);
+      entry.setResource(p);
+    }
+    String parsedResource = xmlParser.encodeResourceToString(b);
+    logger.info(parsedResource);
+    return parsedResource;
+  }
+
+
+  @GetMapping(value = "case-report-bundle.json", produces = "application/fhir+json")
+  public String reportJson() {
+    Bundle b = getReportBundle();
+    String parsedResource = jsonParser.encodeResourceToString(b);
+    return parsedResource;
+  }
+
+  @GetMapping(value = "case-report-bundle.xml", produces = "application/fhir+xml")
+  public String reportXml() {
+    Bundle b = getReportBundle();
+    String parsedResource = xmlParser.encodeResourceToString(b);
+    return parsedResource;
+  }
+
+  @GetMapping(value = "report", produces = "application/fhir+xml")
+  public String report() {
+    Bundle b = getReportBundle();
+    String parsedResource = xmlParser.encodeResourceToString(b);
+    return parsedResource;
+  }
+
+  private HashMap<String, SimplePosition> getCachedGeoCoordinates() {
+    if (!new File("coordinates.txt").exists()) {
+      return new HashMap<String, SimplePosition>();
     }
 
-    public ValidationResult validate(IBaseResource resource) {
-        ValidationResult result = null;
-        FhirValidator validator = ctx.newValidator();
-        IValidatorModule module1 = new SchemaBaseValidator(ctx);
-        validator.registerValidatorModule(module1);
-        IValidatorModule module2 = new SchematronBaseValidator(ctx);
-        validator.registerValidatorModule(module2);
-        result = validator.validateWithResult(resource);
-        for (SingleValidationMessage next : result.getMessages()) {
-            System.out.println(next.getLocationString() + " " + next.getMessage());
+    try {
+      FileInputStream fs = new FileInputStream("coordinates.txt");
+      ObjectInputStream ois = new ObjectInputStream(fs);
+      HashMap<String, SimplePosition> coordinates = (HashMap) ois.readObject();
+      ois.close();
+      fs.close();
+      return coordinates;
+    } catch (Exception ex) {
+      return new HashMap<String, SimplePosition>();
+    }
+  }
+
+  private void setCachedGeoCoordinates(HashMap<String, SimplePosition> coordinates) {
+    try {
+      FileOutputStream fs = new FileOutputStream("coordinates.txt");
+      ObjectOutputStream oos = new ObjectOutputStream(fs);
+      oos.writeObject(coordinates);
+      oos.close();
+      fs.close();
+    } catch (Exception ex) {
+      // Do nothing
+    }
+  }
+
+  @GetMapping("heatmap")
+  private List<SimplePosition> getHeatMapData() throws InterruptedException, ApiException, IOException {
+    Bundle bundle = this.getReportBundle();
+    List<SimplePosition> positions = new ArrayList();
+    GeoApiContext geoContext = new GeoApiContext.Builder()
+      .apiKey("XXX")
+      .build();
+    HashMap<String, SimplePosition> cached = this.getCachedGeoCoordinates();
+
+    for (BundleEntryComponent entry : bundle.getEntry()) {
+      Bundle ecr = (Bundle) entry.getResource();
+      Patient patient = null;
+
+      for (BundleEntryComponent ecrEntry : ecr.getEntry()) {
+        if (ecrEntry.getResource().getResourceType().equals(ResourceType.Patient)) {
+          patient = (Patient) ecrEntry.getResource();
+          break;
         }
-        return result;
-    }
+      }
 
+      if (patient == null) {
+        continue;
+      }
 
-    @GetMapping(value = "patients", produces = "application/fhir+xml")
-    public String patients() {
-        ValueSet vs = vsClient.getValueSet(dxtcSnomedValueSetUrl);
-        logger.info("Retrieved value set", vs.getUrl());
-        List<Condition> resultList = vsClient.conditionCodeQuery(vs);
-        Map<String, Patient> patientRefs = getUniquePatientReferences(resultList);
-        Bundle b = new Bundle();
-        b.setType(BundleType.COLLECTION);
-        for (String key : patientRefs.keySet()) {
-            Patient p = patientRefs.get(key);
-            BundleEntryComponent entry = b.addEntry();
-            entry.setFullUrl(clinicalDataServerBase + "/" + key);
-            entry.setResource(p);
+      for (Address addr : patient.getAddress()) {
+        if (addr.getCity() == null || addr.getCity().isEmpty() || addr.getState() == null || addr.getState().isEmpty()) {
+          continue;   // ignore addresses that don't have a city and state
         }
-        String parsedResource = xmlParser.encodeResourceToString(b);
-        logger.info(parsedResource);
-        return parsedResource;
-    }
-    
 
-    @GetMapping(value = "case-report-bundle.json", produces = "application/fhir+json")
-    public String reportJson() {
-        Bundle b = getReportBundle();
-        String parsedResource = jsonParser.encodeResourceToString(b);
-        return parsedResource;
-    }
+        String address = addr.getCity() + " " + addr.getState();
 
-    @GetMapping(value = "case-report-bundle.xml", produces = "application/fhir+xml")
-    public String reportXml() {
-        Bundle b = getReportBundle();
-        String parsedResource = xmlParser.encodeResourceToString(b);
-        return parsedResource;
-    }
+        if (cached.containsKey(address)) {
+          SimplePosition cachedPosition = cached.get(address);
+          SimplePosition found = null;
 
-    @GetMapping(value = "report", produces = "application/fhir+xml")
-    public String report() {
-        Bundle b = getReportBundle();
-        String parsedResource = xmlParser.encodeResourceToString(b);
-        return parsedResource;
-    }
-
-	private Bundle getReportBundle() {
-		ValueSet vs = vsClient.getValueSet(dxtcCoronavirusValueSetUrl);
-        logger.info("Retrieved value set", vs.getUrl());
-        List<Condition> resultList = vsClient.conditionCodeQuery(vs);
-        Map<String, Patient> patientRefs = getUniquePatientReferences(resultList);
-        Bundle b = new Bundle();
-        // b.setType(BundleType.TRANSACTION);
-        b.setType(BundleType.COLLECTION);
-        for (String key : patientRefs.keySet()) {
-            logger.info("Building report for {}", key);
-            Patient p = patientRefs.get(key);
-            ElectronicCaseReport ecr = new ElectronicCaseReport(this.clinicalDataClient, p, null, null);
-            Bundle ecrDoc = ecr.compile();
-            logger.info("Created report {}", ecrDoc.getId());
-            BundleEntryComponent entry = b.addEntry();
-            entry.setFullUrl(ecrDoc.getId());
-            entry.setResource(ecrDoc);
-            if (b.getType().equals(BundleType.TRANSACTION)) {
-                entry.getRequest().setMethod(HTTPVerb.PUT).setUrl("Bundle/");
+          for (SimplePosition next : positions) {
+            if (next.getLatitude() == cachedPosition.getLatitude() && next.getLongitude() == cachedPosition.getLongitude()) {
+              found = next;
             }
+          }
+
+          if (found == null) {
+            cachedPosition.setStrength(1);
+            positions.add(cachedPosition);
+          } else {
+            found.setStrength(found.getStrength() + 1);
+          }
+          continue;
         }
 
-        // TODO Maven dependency for HAPI FHIR Validation not working, figure out why
+        try {
+          GeocodingResult[] results = GeocodingApi.geocode(geoContext, address).await();
+
+          if (results != null && results.length > 0 && results[0].geometry != null && results[0].geometry.location != null) {
+            SimplePosition position = new SimplePosition();
+            position.setLatitude(results[0].geometry.location.lat);
+            position.setLongitude(results[0].geometry.location.lng);
+            cached.put(address, position);
+            positions.add(position);
+          }
+        } catch (Exception ex) {
+          System.err.println("Error GeoCoding address: " + ex.getMessage());
+        }
+      }
+    }
+
+    this.setCachedGeoCoordinates(cached);
+    return positions;
+  }
+
+  private Bundle getReportBundle() {
+    ValueSet vs = vsClient.getValueSet(dxtcCoronavirusValueSetUrl);
+    logger.info("Retrieved value set", vs.getUrl());
+    List<Condition> resultList = vsClient.conditionCodeQuery(vs);
+    Map<String, Patient> patientRefs = getUniquePatientReferences(resultList);
+    Bundle b = new Bundle();
+    // b.setType(BundleType.TRANSACTION);
+    b.setType(BundleType.COLLECTION);
+    for (String key : patientRefs.keySet()) {
+      logger.info("Building report for {}", key);
+      Patient p = patientRefs.get(key);
+      ElectronicCaseReport ecr = new ElectronicCaseReport(this.clinicalDataClient, p, null, null);
+      Bundle ecrDoc = ecr.compile();
+      logger.info("Created report {}", ecrDoc.getId());
+      BundleEntryComponent entry = b.addEntry();
+      entry.setFullUrl(ecrDoc.getId());
+      entry.setResource(ecrDoc);
+      if (b.getType().equals(BundleType.TRANSACTION)) {
+        entry.getRequest().setMethod(HTTPVerb.PUT).setUrl("Bundle/");
+      }
+    }
+
+    // TODO Maven dependency for HAPI FHIR Validation not working, figure out why
 		/*
 		ValidationResult result = validate(b);
         if (result.isSuccessful()) {
@@ -134,30 +237,30 @@ public class FlintlockController {
         }
 		 */
 
-        logger.info("Finished creating reports");
-		return b;
-	}
+    logger.info("Finished creating reports");
+    return b;
+  }
 
-    @GetMapping(value = "test/{patientId}", produces = "application/xml")
-    public String test(@PathVariable("patientId") String patientId) {
-        Patient subject = (Patient) this.clinicalDataClient
-                .read()
-                .resource(Patient.class)
-                .withId(patientId)
-                .execute();
-        ElectronicCaseReport ecr = new ElectronicCaseReport(this.clinicalDataClient, subject, null, null);
-        Bundle ecrDoc = ecr.compile();
-        IParser xmlParser = this.ctx.newXmlParser();
-        return xmlParser.encodeResourceToString(ecrDoc);
-    }
+  @GetMapping(value = "test/{patientId}", produces = "application/xml")
+  public String test(@PathVariable("patientId") String patientId) {
+    Patient subject = (Patient) this.clinicalDataClient
+      .read()
+      .resource(Patient.class)
+      .withId(patientId)
+      .execute();
+    ElectronicCaseReport ecr = new ElectronicCaseReport(this.clinicalDataClient, subject, null, null);
+    Bundle ecrDoc = ecr.compile();
+    IParser xmlParser = this.ctx.newXmlParser();
+    return xmlParser.encodeResourceToString(ecrDoc);
+  }
 
-    private Map<String, Patient> getUniquePatientReferences(List<Condition> conditions) {
-        HashMap<String, Patient> patients = new HashMap<String, Patient>();
-        for (Condition c : conditions) {
-            String key = c.getSubject().getReference();
-            Patient p = clinicalDataClient.read().resource(Patient.class).withUrl(key).execute();
-            patients.put(key, p);
-        }
-        return patients;
+  private Map<String, Patient> getUniquePatientReferences(List<Condition> conditions) {
+    HashMap<String, Patient> patients = new HashMap<String, Patient>();
+    for (Condition c : conditions) {
+      String key = c.getSubject().getReference();
+      Patient p = clinicalDataClient.read().resource(Patient.class).withUrl(key).execute();
+      patients.put(key, p);
     }
+    return patients;
+  }
 }
---
+++ src/main/java/com/lantanagroup/flintlock/model/SimplePosition.java
@@ -0,0 +1,31 @@
+package com.lantanagroup.flintlock.model;
+
+public class SimplePosition {
+  private double latitude;
+  private double longitude;
+  private int strength = 1;
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+
+  public int getStrength() {
+    return strength;
+  }
+
+  public void setStrength(int strength) {
+    this.strength = strength;
+  }
+}
