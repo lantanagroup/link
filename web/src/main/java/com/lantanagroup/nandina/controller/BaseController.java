d9728ec145ff0b8fb514621e78af643bcbd3d0b8
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2020-06-15 12:34:39 -0700

    Removing code that makes the storage fhir server match the smart-on-fhir fhir server

--- web/src/main/java/com/lantanagroup/nandina/controller/BaseController.java
+++ web/src/main/java/com/lantanagroup/nandina/controller/BaseController.java
@@ -34,22 +34,12 @@ public class BaseController {
 
     protected IGenericClient getFhirStoreClient(Authentication authentication, HttpServletRequest request) throws Exception {
         String fhirBase = Config.getInstance().getFhirServerStoreBase();
-        String token = null;
-
-        if (request.getHeader("fhirBase") != null) {
-            fhirBase = request.getHeader("fhirBase");
-
-            // If fhirBase is passed in header, this request is coming from Smart-on-FHIR launch context and we should
-            // pass the token of the user forward to the FHIR server, rather than use what's in the config.
-            token = (String) authentication.getPrincipal();
-        }
 
         if (Config.getInstance().getRequireHttps() && !fhirBase.contains("https")) {
             throw new Exception(String.format("https is required for FhirClient and was given %s", fhirBase));
         }
 
         IGenericClient fhirClient = this.ctx.newRestfulGenericClient(fhirBase);
-        fhirClient.registerInterceptor(new HapiFhirAuthenticationInterceptor(token));
         return fhirClient;
     }
 }
