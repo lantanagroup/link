033f57e00ef7bdcf719ca80ea59a47c0c064baf7
Rick Geimer <rick.geimer@lantanagroup.com>
2020-06-28 10:25:26 -0400

    Added DeathFilter

---
+++ core/src/main/java/com/lantanagroup/nandina/scoopfilterreport/fhir4/DeathFilter.java
@@ -0,0 +1,40 @@
+package com.lantanagroup.nandina.scoopfilterreport.fhir4;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.hl7.fhir.r4.model.Patient;
+
+public class DeathFilter extends Filter {
+	
+	Date reportDate;
+	
+	public DeathFilter(Date reportDate) {
+		super();
+		this.reportDate = reportDate;
+	}
+
+	@Override
+	public boolean runFilter(PatientData pd) {
+		boolean dead = false;
+
+		Patient p = (Patient) pd.patient;
+		logger.debug("Checking if " + p.getId() + " died");
+		if (p.hasDeceasedDateTimeType()) {
+			Calendar deadDateCal = p.getDeceasedDateTimeType().toCalendar();
+			Calendar reportDateCal = Calendar.getInstance();
+			reportDateCal.setTime(reportDate);
+			dead = sameDay(deadDateCal, reportDateCal);
+		}
+		return dead;
+	}
+
+	protected boolean sameDay(Calendar deadDate, Calendar reportDate) {
+		boolean sameDay = false;
+		if (deadDate.get(Calendar.YEAR) == reportDate.get(Calendar.YEAR)
+				&& deadDate.get(Calendar.DAY_OF_YEAR) == reportDate.get(Calendar.DAY_OF_YEAR))
+			sameDay = true;
+		return sameDay;
+	}
+
+}
