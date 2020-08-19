34e18a61626234c86e80e303b35a78747ca8a5ac
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2020-08-19 11:54:02 -0700

    Replacing SpringSecurityInitializer with manual registration of securityFilter

--- api/src/main/java/com/lantanagroup/nandina/auth/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.lantanagroup.nandina.auth;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-  //no code needed
-}
