39fd4d64701555f63652a90b2d6d9dd6fec78f4a
Brian Norris <brian.norris@lantanagroup.com>
2020-09-08 17:36:57 -0400

    fixing issue with loading value sets from jar file

--- core/src/main/java/com/lantanagroup/nandina/TerminologyHelper.java
+++ core/src/main/java/com/lantanagroup/nandina/TerminologyHelper.java
@@ -2,13 +2,19 @@ package com.lantanagroup.nandina;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
+import org.apache.commons.io.IOUtils;
 import org.hl7.fhir.r4.model.ValueSet;
 import org.hl7.fhir.r4.model.ValueSet.ValueSetExpansionContainsComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.*;
 
@@ -17,7 +23,6 @@ public class TerminologyHelper {
   private FhirContext ctx = FhirContext.forR4();
   private IParser xmlParser = ctx.newXmlParser();
   private Map<String, Set<String>> codeSets = new HashMap<String, Set<String>>();
-
 
   public static String extractCodes(FhirContext fhirContext, File valueSetFile) throws Exception {
     String valueSetFileContents = Files.readString(valueSetFile.toPath());
@@ -102,8 +107,14 @@ public class TerminologyHelper {
         // or look up cannonical http/https value set URIs from a FHIR server.
         // Just because ValueSet.uri starts with http does not mean it is actually accessible at that URL
         throw new Exception("Nandina does not yet support http/https value sets.");
-      } else {
-        File valueSetFile = ResourceUtils.getFile(valueSetUri);
+      } else if (valueSetUri.startsWith("classpath:")) {
+        InputStream is = getClass().getClassLoader().getResourceAsStream(valueSetUri.substring("classpath:".length()));
+        StringWriter sw = new StringWriter();
+        IOUtils.copy(is, sw, Charset.defaultCharset());
+        String vsString = sw.toString();
+        vs = (ValueSet) xmlParser.parseResource(vsString);
+      } else if (valueSetUri.startsWith("file:")) {
+        File valueSetFile = ResourceUtils.getFile(valueSetUri.substring("file:".length()));
         String vsString = new String(Files.readAllBytes(valueSetFile.toPath()));
         vs = (ValueSet) xmlParser.parseResource(vsString);
       }
