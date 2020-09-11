08ae0da821d5fdaae8d747a41d7cb73c3cf8f353
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2020-09-11 13:48:19 -0700

    Implementing a work-around for HAPI not supporting properly implementing the `measure` search parameter; the SANER `PrepareQuery` first searches for all MeasureReports by date, then manually filters the MeasureReport, then retrieves the full MeasureReport in parallel.

--- query/src/main/java/com/lantanagroup/nandina/query/BasePrepareQuery.java
+++ query/src/main/java/com/lantanagroup/nandina/query/BasePrepareQuery.java
@@ -88,6 +88,12 @@ public abstract class BasePrepareQuery implements IPrepareQuery {
             resMap.put(res.getIdElement().getIdPart(), res);
         }
         if (b.getLink(Bundle.LINK_NEXT) != null) {
+            // Make sure the next url has the correct base url. Some HAPI implementations don't configure this correctly, and it's easy to work around.
+            String nextUrl = b.getLink(Bundle.LINK_NEXT).getUrl();
+            nextUrl = nextUrl.substring(nextUrl.indexOf("?"));
+            b.getLink(Bundle.LINK_NEXT).setUrl(fhirClient.getServerBase() + nextUrl);
+
+            // Request the next page for the bundle.
             Bundle nextPage = fhirClient.loadPage().next(b).execute();
             resMap.putAll(bundleToMap(nextPage));
         }
--- query/src/main/java/com/lantanagroup/nandina/query/fhir/r4/saner/PrepareQuery.java
+++ query/src/main/java/com/lantanagroup/nandina/query/fhir/r4/saner/PrepareQuery.java
@@ -2,20 +2,23 @@ package com.lantanagroup.nandina.query.fhir.r4.saner;
 
 import com.lantanagroup.nandina.Helper;
 import com.lantanagroup.nandina.query.BasePrepareQuery;
+import org.hl7.fhir.r4.model.MeasureReport;
 import org.hl7.fhir.r4.model.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class PrepareQuery extends BasePrepareQuery {
+  protected static final Logger logger = LoggerFactory.getLogger(PrepareQuery.class);
+
   @Override
   public void execute() {
     String reportDate = this.criteria.get("reportDate");
     String overflowLocations = this.criteria.get("overflowLocations");
     Map<String, String> queryCriteria = (Map<String, String>) this.contextData.get("queryCriteria");
 
-    String url = String.format("MeasureReport?measure=%s&date=%s&",
-            Helper.URLEncode(Constants.MEASURE_URL),
-            Helper.URLEncode(reportDate));
+    String url = String.format("MeasureReport?_summary=true&date=%s&", Helper.URLEncode(reportDate));
 
     if (overflowLocations != null && !overflowLocations.isEmpty()) {
       url += String.format("subject=%s&", overflowLocations);
@@ -27,7 +30,31 @@ public class PrepareQuery extends BasePrepareQuery {
       }
     }
 
+    logger.debug("Searching for MeasureReport instances by date");
+
     Map<String, Resource> results = this.search(url);
+
+    logger.debug(String.format("Found %s MeasureReport entries in summary mode. Retieving full MeasureReprot resources.", results.size()));
+
+    for (String measureReportId : results.keySet()) {
+      MeasureReport measureReport = (MeasureReport) results.get(measureReportId);
+
+      if (measureReport.getMeasure() == null || !measureReport.getMeasure().equalsIgnoreCase(Constants.MEASURE_URL)) {
+        results.remove(measureReportId);
+      }
+    }
+
+    logger.debug(String.format("%s MeasureReport entries remain after filtering by 'measure'. Retrieving full MeasureReports...", results.size()));
+
+    results.keySet().parallelStream().forEach(measureReportId -> {
+      MeasureReport measureReport = fhirClient
+              .read()
+              .resource(MeasureReport.class)
+              .withId(measureReportId)
+              .execute();
+      results.put(measureReportId, measureReport);
+    });
+
     this.addContextData("measureReportData", results);
   }
 }
