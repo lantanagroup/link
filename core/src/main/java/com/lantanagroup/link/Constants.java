62f28a7d81aed901cc71f6cc636c70734c7615fe
LANTANAGROUP\ariana.mihailescu <ariana.mihailescu@lantanagroup.com>
2021-07-15 18:05:50 -0400

    NHSNLINK-462: Create and retrieve user Practitioner resources with specific tags

--- core/src/main/java/com/lantanagroup/link/Constants.java
+++ core/src/main/java/com/lantanagroup/link/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
   public static final String DocRefCode = "55186-1";
   public static final String DocRefDisplay = "Measure Document";
   public static final String ReportPositionExtUrl = "http://hl7.org/fhir/uv/saner/StructureDefinition/GeoLocation";
+  public static final String LinkUserTag = "link-user";
 }
