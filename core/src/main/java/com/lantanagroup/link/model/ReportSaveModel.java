a8e0db3dc37ed0cb7e6593191c4ce94039001e71
ajagann1 <arvind.jagannathan@lantanagroup.com>
2021-08-17 16:41:07 -0400

    NHSNLINK-435: Added in custom deserialization.

--- core/src/main/java/com/lantanagroup/link/model/ReportSaveModel.java
+++ core/src/main/java/com/lantanagroup/link/model/ReportSaveModel.java
@@ -1,5 +1,7 @@
 package com.lantanagroup.link.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.lantanagroup.link.serialize.ReportSaveModelDeserializer;
 import lombok.Getter;
 import lombok.Setter;
 import org.hl7.fhir.r4.model.MeasureReport;
@@ -7,6 +9,9 @@ import org.hl7.fhir.r4.model.QuestionnaireResponse;
 
 @Getter @Setter
 public class ReportSaveModel {
+    @JsonDeserialize(using = ReportSaveModelDeserializer.class)
     MeasureReport measureReport;
+
+    @JsonDeserialize(using = ReportSaveModelDeserializer.class)
     QuestionnaireResponse questionnaireResponse;
 }
