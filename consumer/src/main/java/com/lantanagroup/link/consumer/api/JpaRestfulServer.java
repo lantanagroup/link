dc8622a5e760eb3d8886e05b8300423cefd60fc5
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2021-09-19 11:06:53 -0700

    Adding consumer project that starts up a basic HAPI R4 FHIR server. Still need to make it configurable for MySQL vs H2

---
+++ consumer/src/main/java/com/lantanagroup/link/consumer/api/JpaRestfulServer.java
@@ -0,0 +1,114 @@
+package com.lantanagroup.link.consumer.api;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.IValidationSupport;
+import ca.uhn.fhir.jpa.api.config.DaoConfig;
+import ca.uhn.fhir.jpa.api.dao.IFhirSystemDao;
+import ca.uhn.fhir.jpa.cache.ResourceChangeListenerCacheFactory;
+import ca.uhn.fhir.jpa.cache.ResourceChangeListenerRegistryImpl;
+import ca.uhn.fhir.jpa.config.HibernatePropertiesProvider;
+import ca.uhn.fhir.jpa.model.entity.ModelConfig;
+import ca.uhn.fhir.jpa.provider.JpaCapabilityStatementProvider;
+import ca.uhn.fhir.jpa.searchparam.matcher.InMemoryResourceMatcher;
+import ca.uhn.fhir.jpa.searchparam.registry.SearchParamRegistryImpl;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.server.provider.ResourceProviderFactory;
+import ca.uhn.fhir.rest.server.util.ISearchParamRegistry;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.h2.Driver;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.jdbc.dialect.internal.StandardDialectResolver;
+import org.hibernate.engine.jdbc.dialect.spi.DatabaseMetaDataDialectResolutionInfoAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class JpaRestfulServer extends RestfulServer {
+  @Autowired
+  ApplicationContext applicationContext;
+
+  @Autowired
+  IFhirSystemDao fhirSystemDao;
+
+  @Autowired
+  DaoConfig daoConfig;
+
+  @Autowired
+  private IValidationSupport validationSupport;
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Autowired
+  private ModelConfig modelConfig;
+
+  @Autowired
+  ResourceProviderFactory resourceProviders;
+
+  private FhirContext fhirContext;
+  private SearchParamRegistryImpl searchParamRegistry = new SearchParamRegistryImpl();
+  private ResourceChangeListenerRegistryImpl resourceChangeListenerRegistry = new ResourceChangeListenerRegistryImpl();
+  private InMemoryResourceMatcher inMemoryResourceMatcher = new InMemoryResourceMatcher();
+
+  public JpaRestfulServer() {
+  }
+
+  @Override
+  protected void initialize() throws ServletException {
+    super.initialize();
+
+    this.fhirContext = FhirContext.forR4();
+    this.setFhirContext(this.fhirContext);
+
+    this.resourceChangeListenerRegistry.setFhirContext(this.fhirContext);
+    this.resourceChangeListenerRegistry.setResourceChangeListenerCacheFactory(this.applicationContext.getBean(ResourceChangeListenerCacheFactory.class));
+    this.resourceChangeListenerRegistry.setInMemoryResourceMatcher(new InMemoryResourceMatcher());
+
+    this.searchParamRegistry.setFhirContext(this.fhirContext);
+    this.searchParamRegistry.setModelConfig(this.modelConfig);
+    this.searchParamRegistry.setResourceChangeListenerRegistry(this.resourceChangeListenerRegistry);
+    this.searchParamRegistry.registerListener();
+    this.searchParamRegistry.handleInit(new ArrayList<>());
+
+    this.registerProviders(this.resourceProviders.createProviders());
+
+    JpaCapabilityStatementProvider confProvider = new JpaCapabilityStatementProvider(this, this.fhirSystemDao, this.daoConfig, this.searchParamRegistry, validationSupport);
+    this.setServerConformanceProvider(confProvider);
+  }
+
+  @Bean(destroyMethod = "close")
+  public BasicDataSource dataSource() {
+    BasicDataSource retVal = new BasicDataSource();
+    Driver driver = new Driver();
+    retVal.setDriver(driver);
+    retVal.setUrl("jdbc:h2:file:./target/database/h2");
+    retVal.setUsername("sa");
+    retVal.setPassword(null);
+    return retVal;
+  }
+
+  @Bean
+  public ISearchParamRegistry searchParamRegistry() {
+    return this.searchParamRegistry;
+  }
+
+  @Primary
+  @Bean
+  public HibernatePropertiesProvider jpaStarterDialectProvider(LocalContainerEntityManagerFactoryBean myEntityManagerFactory) throws SQLException {
+    DatabaseMetaData metaData = this.dataSource.getConnection().getMetaData();
+    return new HibernatePropertiesProvider() {
+      @Override
+      public Dialect getDialect() {
+        return new StandardDialectResolver().resolveDialect(new DatabaseMetaDataDialectResolutionInfoAdapter(metaData));
+      }
+    };
+  }
+}
