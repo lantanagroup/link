646e256f27f34a3a2d3ff30cbd99456cb5412b9c
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2021-09-23 13:18:08 -0700

    Improvements to unit tests

--- core/src/test/java/com/lantanagroup/link/FhirHelperTests.java
+++ core/src/test/java/com/lantanagroup/link/FhirHelperTests.java
@@ -17,15 +17,20 @@ public class FhirHelperTests {
     HumanName name2 = new HumanName().addGiven("Joe Sombody");
     HumanName name3 = new HumanName().setFamily("Joe Sombody");
     HumanName name4 = new HumanName().setText("Joe Sombody");
+    HumanName name5 = new HumanName();
 
     String actual1 = FhirHelper.getName(Arrays.asList(name1));
     String actual2 = FhirHelper.getName(Arrays.asList(name2));
     String actual3 = FhirHelper.getName(Arrays.asList(name3));
     String actual4 = FhirHelper.getName(Arrays.asList(name4));
+    String actual5 = FhirHelper.getName(Arrays.asList(name5));
+    String actual6 = FhirHelper.getName(Arrays.asList());
 
     Assert.assertEquals(actual1, "Joe Sombody");
     Assert.assertEquals(actual2, "Joe Sombody");
     Assert.assertEquals(actual3, "Joe Sombody");
     Assert.assertEquals(actual4, "Joe Sombody");
+    Assert.assertEquals(actual5, "Unknown");
+    Assert.assertEquals(actual6, "Unknown");
   }
 }
