19e1aaf98586abf5520a80e8659260dabc6b5bea
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2022-01-04 09:23:05 -0800

    Adding package for agent to scan

--- agent/src/main/java/com/lantanagroup/link/agent/AgentApplication.java
+++ agent/src/main/java/com/lantanagroup/link/agent/AgentApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication(scanBasePackages = {
+        "com.lantanagroup.link.agent",
         "com.lantanagroup.link.query",
         "com.lantanagroup.link.config.query",
         "com.lantanagroup.link.auth"
