c3e806fa88d01ad3402250d651db4de73d9b460b
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2022-01-10 12:43:33 -0800

    adding class and bean to execute only a single shell command and NOT use interactive mode

---
+++ cli/src/main/java/com/lantanagroup/link/cli/SingleCommandShellRunner.java
@@ -0,0 +1,52 @@
+package com.lantanagroup.link.cli;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.shell.Input;
+import org.springframework.shell.InputProvider;
+import org.springframework.shell.Shell;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Order(0)
+public class SingleCommandShellRunner implements ApplicationRunner {
+  private final ConfigurableEnvironment environment;
+  private final Shell shell;
+  private final List<String> commands = List.of("query", "generate-and-submit");
+
+  public SingleCommandShellRunner(Shell shell, ConfigurableEnvironment environment) {
+    this.shell = shell;
+    this.environment = environment;
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    Optional<String> command = args.getNonOptionArgs().stream().filter(a -> commands.indexOf(a) >= 0).findFirst();
+
+    if (!command.isPresent()) {
+      return;
+    }
+
+    List<String> commandAndArgs = new ArrayList<>();
+    boolean afterCommand = false;
+
+    for (String sourceArg : args.getSourceArgs()) {
+      if (afterCommand) {
+        commandAndArgs.add(sourceArg);
+      } else if (sourceArg.equals(command.get())) {
+        afterCommand = true;
+        commandAndArgs.add(command.get());
+      }
+    }
+
+    InteractiveShellApplicationRunner.disable(this.environment);
+    // TODO: shell.run()?
+  }
+}
