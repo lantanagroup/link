a96c3985274264395bf01c5bcc0bdea0c2b0aabe
saramunich <sara.munich@lantanagroup.com>
2022-03-07 11:28:25 -0500

    Created QueryResponse class.

---
+++ core/src/main/java/com/lantanagroup/link/model/QueryResponse.java
@@ -0,0 +1,17 @@
+package com.lantanagroup.link.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hl7.fhir.r4.model.Bundle;
+
+@Getter
+@Setter
+public class QueryResponse {
+  String patientId;
+  Bundle bundle;
+
+  public QueryResponse(String patientId, Bundle bundle) {
+    this.patientId = patientId;
+    this.bundle = bundle;
+  }
+}
