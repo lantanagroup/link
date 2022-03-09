71a3d5f0689ef26191a0dc540d869359a3b9f008
LANTANAGROUP\ariana.mihailescu <ariana.mihailescu@lantanagroup.com>
2022-03-09 15:44:37 -0500

    NHSNLINK-686: AuthInterceptor uses roles for access to resources

--- api/src/main/java/com/lantanagroup/link/api/auth/LinkAuthManager.java
+++ api/src/main/java/com/lantanagroup/link/api/auth/LinkAuthManager.java
@@ -1,7 +1,6 @@
 package com.lantanagroup.link.api.auth;
 
 import com.lantanagroup.link.auth.OAuth2Helper;
-import com.lantanagroup.link.config.api.ApiConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,18 +12,18 @@ import org.springframework.security.core.AuthenticationException;
 public class LinkAuthManager implements AuthenticationManager {
   private static final Logger logger = LoggerFactory.getLogger(LinkAuthManager.class);
 
-  public LinkAuthManager () {
+  public LinkAuthManager() {
   }
 
   @Override
-  public Authentication authenticate (Authentication authentication) throws AuthenticationException {
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
     String authHeader = (String) authentication.getCredentials();
 
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
       throw new BadCredentialsException("This REST operation requires a Bearer Authorization header.");
     }
 
-    authentication.setAuthenticated(OAuth2Helper.validateAuthHeader(authHeader));
+    authentication.setAuthenticated(OAuth2Helper.validateAuthHeader(authHeader) != null ? true : false);
 
     return authentication;
   }
