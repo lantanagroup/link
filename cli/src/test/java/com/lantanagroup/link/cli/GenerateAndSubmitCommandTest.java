f93f6897a061fd03746d86362c19d462a9c3faf0
saramunich <sara.munich@lantanagroup.com>
2022-04-13 13:28:16 -0400

    Fixed issue with junit dependencies for the cli

---
+++ cli/src/test/java/com/lantanagroup/link/cli/GenerateAndSubmitCommandTest.java
@@ -0,0 +1,20 @@
+package com.lantanagroup.link.cli;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GenerateAndSubmitCommandTest {
+
+  @Test
+  public void getConfigInfo() {
+    int x = 0;
+    int y = 9;
+  }
+
+  @Test
+  public void setConfigInfo() {
+    int x = 0;
+    int y = 9;
+  }
+}
