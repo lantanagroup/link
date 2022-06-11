f71797e68a0c60fb4715ad05f9c5c26ce41de86b
LANTANAGROUP\ariana.mihailescu <ariana.mihailescu@lantanagroup.com>
2022-06-11 11:55:05 -0400

    LINK-950: Create IReportAggregator interface

---
+++ core/src/main/java/com/lantanagroup/link/model/StoredReportDefinition.java
@@ -0,0 +1,16 @@
+package com.lantanagroup.link.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents a
+ */
+@Getter
+@Setter
+public class StoredReportDefinition {
+  private String id;
+  private String name;
+  private String system;
+  private String value;
+}
