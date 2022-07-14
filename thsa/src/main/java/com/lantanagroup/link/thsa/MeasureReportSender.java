127ffce7cbc3dfe077932d651e445dfda3e6fcf8
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2022-07-14 07:32:43 -0700

    Implementation of THSA's `MeasureReport` sender Updating AuthInterceptor in `consumer` to allow metadata for all requests

--- thsa/src/main/java/com/lantanagroup/link/thsa/MeasureReportSender.java
+++ thsa/src/main/java/com/lantanagroup/link/thsa/MeasureReportSender.java
@@ -1,18 +1,25 @@
 package com.lantanagroup.link.thsa;
 
 import com.lantanagroup.link.FhirDataProvider;
+import com.lantanagroup.link.GenericSender;
 import com.lantanagroup.link.IReportSender;
+import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.DocumentReference;
 import org.hl7.fhir.r4.model.MeasureReport;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class MeasureReportSender implements IReportSender {
+@Component
+public class MeasureReportSender extends GenericSender implements IReportSender {
   @Override
   public void send(MeasureReport masterMeasureReport, DocumentReference documentReference, HttpServletRequest request, Authentication auth, FhirDataProvider fhirDataProvider, Boolean sendWholeBundle, boolean removeGeneratedObservations) throws Exception {
-    // TODO: Construct new FhirDataProvider for the consumer (take into account authentication)
+    this.sendContent(masterMeasureReport, documentReference, fhirDataProvider);
+  }
 
-    // TODO: updateResource(masterMeasureReport) on FhirDataProvider for consumer
+  @Override
+  public String bundle(Bundle bundle, FhirDataProvider fhirProvider, String type) {
+    return null;
   }
 }
