e6e4e4282180aea21995f249f48f340c601d0175
saramunich <sara.munich@lantanagroup.com>
2022-07-14 16:42:08 -0400

    Added funtionality to MeasureStoreController to store the measureBundle as a xml file on the current working directory.

--- measureeval/src/main/java/com/lantanagroup/link/measureeval/controller/MeasureStoreController.java
+++ measureeval/src/main/java/com/lantanagroup/link/measureeval/controller/MeasureStoreController.java
@@ -1,6 +1,8 @@
 package com.lantanagroup.link.measureeval.controller;
 
 import ca.uhn.fhir.context.FhirContext;
+import com.lantanagroup.link.FhirContextProvider;
+import com.lantanagroup.link.FhirDataProvider;
 import com.lantanagroup.link.measureeval.config.MeasureEvalConfig;
 import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,14 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class MeasureStoreController {
@@ -26,6 +36,18 @@ public class MeasureStoreController {
   @PostMapping("/$store-measure")
   public void storeMeasure(@RequestBody Bundle measureBundle) {
     // TODO: Store Bundle on file system in a file named <measureBundle.id>.xml
+    FhirContext ctx = FhirContextProvider.getFhirContext();
+    String fileName = measureBundle.getId() + ".xml";
+    File file = new File(fileName);
+    String measureContentXML = ctx.newXmlParser().encodeResourceToString(measureBundle);
+    try(FileWriter fw=new FileWriter(file))
+    {
+      fw.write(measureContentXML);
+      fw.flush();
+    }catch(IOException ex)
+    {
+      ex.printStackTrace();
+    }
     System.out.println(this.config.getMeasuresPath());
   }
 }
