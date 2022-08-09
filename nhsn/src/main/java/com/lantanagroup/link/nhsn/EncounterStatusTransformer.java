6f3ba49a68fc3f3cd6cd74c2e8f954af7e13fbd8
saramunich <sara.munich@lantanagroup.com>
2022-08-08 17:03:33 -0700

    Modified EncounterStatusTransformer to look through the patient encounter resources and update their status to "finished" if their period has an end.

--- nhsn/src/main/java/com/lantanagroup/link/nhsn/EncounterStatusTransformer.java
+++ nhsn/src/main/java/com/lantanagroup/link/nhsn/EncounterStatusTransformer.java
@@ -4,9 +4,16 @@ import com.lantanagroup.link.FhirDataProvider;
 import com.lantanagroup.link.IReportGenerationEvent;
 import com.lantanagroup.link.config.api.ApiConfig;
 import com.lantanagroup.link.config.api.ApiConfigEvents;
+import com.lantanagroup.link.model.PatientOfInterestModel;
+import com.lantanagroup.link.model.QueryResponse;
 import com.lantanagroup.link.model.ReportContext;
 import com.lantanagroup.link.model.ReportCriteria;
 import lombok.Setter;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Encounter;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.ResourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +34,28 @@ public class EncounterStatusTransformer implements IReportGenerationEvent {
   @Override
   public void execute(ReportCriteria reportCriteria, ReportContext context, ApiConfig config, FhirDataProvider fhirDataProvider) {
 
-    List<String> encounters = null;
+    for (PatientOfInterestModel patientOfInterest : context.getPatientsOfInterest()) {
+      logger.info("Patient is: " + patientOfInterest.getId());
+      try {
+        Bundle patientBundle = fhirDataProvider.getBundleById(context.getReportId() + "-" + patientOfInterest.getId().hashCode());
+        for(Bundle.BundleEntryComponent patientResource : patientBundle.getEntry()) {
+          if(patientResource.getResource().getResourceType().equals(ResourceType.Encounter)) {
+            logger.info("Patient encounter is: " + patientResource.getResource().getId());
+            Encounter patientEncounter = (Encounter)patientResource.getResource();
+            if(patientEncounter.getPeriod().hasEnd()) {
+              patientEncounter.setStatus(Encounter.EncounterStatus.FINISHED);
+            }
+          }
+        }
+
+      }
+      catch (Exception ex) {
+        logger.error("Exception is: " + ex.getMessage());
+      }
+
+    }
+
+    /*List<String> encounters = null;
     try {
       Method eventMethodInvoked = ApiConfigEvents.class.getMethod("getAfterPatientDataQuery");
       encounters = (List<String>) eventMethodInvoked.invoke(apiConfigEvents);
@@ -38,6 +66,6 @@ public class EncounterStatusTransformer implements IReportGenerationEvent {
       for (String encounter : encounters) {
 
       }
-    }
+    }*/
   }
 }
