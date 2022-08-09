4cbc189da1728f6cb941ebb7783a2448bf4e30e3
saramunich <sara.munich@lantanagroup.com>
2022-08-09 11:37:21 -0700

    Set the url for the previous status extention to https://www.lantanagroup.com/fhir/StructureDefinition/nhsn-encounter-original-status

--- nhsn/src/main/java/com/lantanagroup/link/nhsn/EncounterStatusTransformer.java
+++ nhsn/src/main/java/com/lantanagroup/link/nhsn/EncounterStatusTransformer.java
@@ -1,27 +1,15 @@
 package com.lantanagroup.link.nhsn;
 
-import com.auth0.jwt.JWT;
 import com.lantanagroup.link.Constants;
 import com.lantanagroup.link.FhirDataProvider;
-import com.lantanagroup.link.FhirHelper;
 import com.lantanagroup.link.IReportGenerationEvent;
 import com.lantanagroup.link.config.api.ApiConfig;
-import com.lantanagroup.link.config.api.ApiConfigEvents;
 import com.lantanagroup.link.model.PatientOfInterestModel;
-import com.lantanagroup.link.model.QueryResponse;
 import com.lantanagroup.link.model.ReportContext;
 import com.lantanagroup.link.model.ReportCriteria;
-import lombok.Setter;
-import org.apache.catalina.connector.Request;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
 
 
 
@@ -41,9 +29,12 @@ public class EncounterStatusTransformer implements IReportGenerationEvent {
             Encounter patientEncounter = (Encounter)patientResource.getResource();
             if(patientEncounter.getPeriod().hasEnd()) {
               Extension previous = new Extension();
-              previous.setValue(patientEncounter.getClass_());
+              previous.setUrl(Constants.OriginalEncounterStatus);
+              Coding coding = new Coding();
+              coding.setCode(patientEncounter.getStatus().toString());
+              previous.setValue(coding);
               patientEncounter.setStatus(Encounter.EncounterStatus.FINISHED);
-              patientResource.addExtension(previous);
+              patientEncounter.addExtension(previous);
               patientResource.setResource(patientEncounter);
               //fhirDataProvider.audit(new Request(), new JWT(), FhirHelper.AuditEventTypes.Export, "Recording transformation of encounter status.");
             }
