dcb9766c83feda26365918574fec293d739ba370
Steven Williams <steven.williams@lantanagroup.com>
2022-08-10 11:58:43 -0400

    Refactor error-handling infrastructure
    Reduced priority of ErrorFilter, allowing it to handle exceptions before
    they're swallowed by ErrorPageFilter.  Added logic for internally
    resolving specific exception types.  Removed ErrorInfo and moved logic
    into ErrorController.

--- core/src/main/java/com/lantanagroup/link/spring/ErrorController.java
+++ core/src/main/java/com/lantanagroup/link/spring/ErrorController.java
@@ -1,30 +1,65 @@
 package com.lantanagroup.link.spring;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
+@RequestMapping({"${server.error.path:${error.path:/error}}"})
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-  @Value("${server.error.path:/error}")
-  private String errorPath;
+  private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+  private static final String MESSAGE_WITHOUT_ID = "An error occurred while processing your request; "
+          + "for further assistance, please contact your system administrator";
+  private static final String MESSAGE_WITH_ID = MESSAGE_WITHOUT_ID + " with the following error ID: %s";
+
+  @Value("${error.path:/error}")
+  private String errorPath = "/error";
 
   @Override
   public String getErrorPath() {
     return errorPath;
   }
 
-  @RequestMapping("${server.error.path:/error}")
-  public Map<String, Object> handleError(HttpServletRequest request, HttpServletResponse response) {
-    ErrorInfo errorInfo = ErrorInfo.retrieve(request);
-    if (errorInfo == null) {
-      errorInfo = new ErrorInfo(HttpStatus.valueOf(response.getStatus()));
+  @RequestMapping
+  public Map<String, Object> handle(HttpServletRequest request, HttpServletResponse response) {
+    Map<String, Object> properties = new LinkedHashMap<>();
+    properties.put("timestamp", TIMESTAMP_FORMAT.format(new Date()));
+    properties.put("status", response.getStatus());
+    String message = getMessage(request, response);
+    if (StringUtils.isNotEmpty(message)) {
+      properties.put("error", message);
     }
-    return errorInfo.getProperties();
+    String path = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+    if (StringUtils.isNotEmpty(path)) {
+      properties.put("path", path);
+    }
+    return properties;
+  }
+
+  private String getMessage(HttpServletRequest request, HttpServletResponse response) {
+    if (response.getStatus() == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+      UUID errorId = (UUID) request.getAttribute(ErrorFilter.ERROR_ID);
+      return errorId != null ? String.format(MESSAGE_WITH_ID, errorId) : MESSAGE_WITHOUT_ID;
+    }
+    String message = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+    if (StringUtils.isNotEmpty(message)) {
+      return message;
+    }
+    HttpStatus status = HttpStatus.resolve(response.getStatus());
+    if (status != null) {
+      return status.getReasonPhrase();
+    }
+    return null;
   }
 }
