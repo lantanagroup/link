afd2af422fbc73c843136643cf694031109f4c6e
saramunich <sara.munich@lantanagroup.com>
2022-09-06 12:23:39 -0700

    I gave data-governance its own section.

--- core/src/main/java/com/lantanagroup/link/config/DataGovernance/dataGovernanceConfig.java
+++ core/src/main/java/com/lantanagroup/link/config/DataGovernance/dataGovernanceConfig.java
@@ -1,22 +1,22 @@
-package com.lantanagroup.link.config.api;
-
+package com.lantanagroup.link.config.DataGovernance;
 
 import com.lantanagroup.link.config.YamlPropertySourceFactory;
-import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.validation.annotation.Validated;
 
-@Data
+@Getter
+@Setter
 @Configuration
-@ConfigurationProperties(prefix = "api.data-governance")
+@ConfigurationProperties(prefix = "data-governance")
 @Validated
 @PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
-public class ApiDataGovernance {
+public class dataGovernanceConfig {
   /**
-   * <strong>api.data-governance.retention-period</strong><br>Contains the retention periods for the census list, patient data, and the report.
+   * <strong>data-governance.retention-period</strong><br>Contains the retention periods for the census list, patient data, and the report.
    */
   @Getter
   private RetentionPeriod retentionPeriod = new RetentionPeriod();
@@ -24,19 +24,19 @@ public class ApiDataGovernance {
 
 class RetentionPeriod {
   /**
-   * <strong>api.data-governance.retention-period.census-list-retention</strong><br>Contains the retention period for the census list.
+   * <strong>data-governance.retention-period.census-list-retention</strong><br>Contains the retention periods for the census list.
    */
   @Getter
   private String censusListRetention;
 
   /**
-   * <strong>api.data-governance.retention-period.patient-data-retention</strong><br>Contains the retention period for the patient data.
+   * <strong>data-governance.retention-period.patient-data-retention</strong><br>Contains the retention periods for the patient data.
    */
   @Getter
   private String patientDataRetention;
 
   /**
-   * <strong>api.data-governance.retention-period.report-retention</strong><br>Contains the retention period for the report.
+   * <strong>data-governance.retention-period.report-retention</strong><br>Contains the retention periods for the report.
    */
   @Getter
   private String reportRetention;
