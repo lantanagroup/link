06a38ef2feabe9d9695cb92b0a5eee64ecd157a4
LANTANAGROUP\ariana.mihailescu <ariana.mihailescu@lantanagroup.com>
2022-09-22 13:58:22 -0400

    LINK-1158: Improve function to build Master MeasureReports for Multi-measure - UI side

--- core/src/main/java/com/lantanagroup/link/IdentifiersHelper.java
+++ core/src/main/java/com/lantanagroup/link/IdentifiersHelper.java
@@ -2,13 +2,13 @@ package com.lantanagroup.link;
 
 import com.lantanagroup.link.model.ReportCriteria;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class IdentifiersHelper {
 
   public static String getMasterIdentifierValue(ReportCriteria criteria) {
-    List components = new ArrayList(criteria.getBundleIds());
+    List components = new LinkedList(criteria.getBundleIds());
     components.add(criteria.getPeriodStart());
     components.add(criteria.getPeriodEnd());
     return Helper.getHashedValue(components);
@@ -20,5 +20,20 @@ public class IdentifiersHelper {
 
   public static String getIndividualMeasureReportId(String masterMeasureReportId, String patientId) {
     return masterMeasureReportId + "-" + patientId.hashCode();
+  }
+
+  public static String getPatientBundleId(String masterIdentifierValue, String patientId) {
+    return masterIdentifierValue + "-" + patientId.hashCode();
+  }
+
+  public static String getMasterMeasureReportId(String reportId) {
+    String masterMeasureReportId = "";
+
+    if (reportId.charAt(0) == '-') {
+      masterMeasureReportId = '-' + reportId.substring(1, reportId.substring(1).indexOf("-") + 1);
+    } else {
+      masterMeasureReportId = reportId.substring(0, reportId.indexOf("-"));
+    }
+    return masterMeasureReportId;
   }
 }
