2fb3fdf82cf49b7ebdb45aa87873a57f36546cd7
saramunich <sara.munich@lantanagroup.com>
2022-10-19 11:31:41 -0700

    got it to use the correct default times

--- web/src/main/angular/app/generate/generate.component.ts
+++ web/src/main/angular/app/generate/generate.component.ts
@@ -55,10 +55,10 @@ export class GenerateComponent implements OnInit {
   }
 
   onDefaultTimeSelect() {
-    //const defaultStart = new Date(1900, 1, 1);
-    const defaultStart = {year: 1900, month: 1, day: 1};
-    this.startDate = getFhirDate(defaultStart);
-    this.endDate = getFhirDate(getFhirNow());
+    this.criteria.periodStart = getFhirDate({year: 1900, month: 1, day: 1});
+    this.onStartDateSelected();
+    this.criteria.periodEnd = getFhirDate(getFhirNow());
+    this.onEndDateSelected();
   }
 
   async reload() {
