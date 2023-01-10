cee556e4d1072e35fd6dc00f62cb4122ae683946
Steven Williams <steven.williams@lantanagroup.com>
2023-01-10 13:38:51 -0500

    Refresh query authorization on 401 response

--- query/src/main/java/com/lantanagroup/link/query/auth/HapiFhirAuthenticationInterceptor.java
+++ query/src/main/java/com/lantanagroup/link/query/auth/HapiFhirAuthenticationInterceptor.java
@@ -1,23 +1,29 @@
 package com.lantanagroup.link.query.auth;
 
-import ca.uhn.fhir.rest.client.api.IClientInterceptor;
+import ca.uhn.fhir.interceptor.api.Hook;
+import ca.uhn.fhir.interceptor.api.Interceptor;
+import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.rest.client.api.IHttpRequest;
 import ca.uhn.fhir.rest.client.api.IHttpResponse;
 import com.lantanagroup.link.config.query.QueryConfig;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 import java.io.IOException;
 
-public class HapiFhirAuthenticationInterceptor implements IClientInterceptor {
+@Interceptor
+public class HapiFhirAuthenticationInterceptor {
   private static final Logger logger = LoggerFactory.getLogger(HapiFhirAuthenticationInterceptor.class);
+  private final ICustomAuth authorizer;
   private String authHeader;
   private String apiKey;
 
   public HapiFhirAuthenticationInterceptor(QueryConfig queryConfig, ApplicationContext context) throws ClassNotFoundException {
     if (StringUtils.isEmpty(queryConfig.getAuthClass())) {
+      authorizer = null;
       return;
     }
 
@@ -26,8 +32,14 @@ public class HapiFhirAuthenticationInterceptor implements IClientInterceptor {
 
     // Get an instance of the class using Spring so that it injects/autowires
     logger.debug(String.format("Getting an instance of the auth class \"%s\" from Spring", queryConfig.getAuthClass()));
-    ICustomAuth authorizer = (ICustomAuth) context.getBean(authClass);
+    authorizer = (ICustomAuth) context.getBean(authClass);
+    refresh();
+  }
 
+  private void refresh() {
+    if (authorizer == null) {
+      return;
+    }
     try {
       logger.debug("Requesting Authorization header from auth class");
       this.authHeader = authorizer.getAuthHeader();
@@ -37,18 +49,46 @@ public class HapiFhirAuthenticationInterceptor implements IClientInterceptor {
     }
   }
 
-  @Override
-  public void interceptRequest(IHttpRequest iHttpRequest) {
+  private void removeHeaders(IHttpRequest request) {
+    request.removeHeaders("Authorization");
+    request.removeHeaders("apikey");
+  }
+
+  private void addHeaders(IHttpRequest request) {
     if (this.authHeader != null && !this.authHeader.isEmpty()) {
-      iHttpRequest.addHeader("Authorization", this.authHeader);
+      request.addHeader("Authorization", this.authHeader);
     }
     if (this.apiKey != null && !this.apiKey.isEmpty()) {
-      iHttpRequest.addHeader("apikey", this.apiKey);
+      request.addHeader("apikey", this.apiKey);
     }
   }
 
-  @Override
-  public void interceptResponse(IHttpResponse iHttpResponse) throws IOException {
-    // nothing
+  private void copyResponse(HttpResponse source, HttpResponse target) {
+    target.setStatusLine(source.getStatusLine());
+    target.setEntity(source.getEntity());
+    target.setLocale(source.getLocale());
+  }
+
+  @Hook(Pointcut.CLIENT_REQUEST)
+  public void interceptRequest(IHttpRequest request) {
+    addHeaders(request);
+  }
+
+  @Hook(Pointcut.CLIENT_RESPONSE)
+  public void interceptResponse(IHttpRequest request, IHttpResponse response) throws IOException {
+    if (response.getStatus() != 401) {
+      return;
+    }
+    if (!(response.getResponse() instanceof HttpResponse)) {
+      return;
+    }
+    refresh();
+    removeHeaders(request);
+    addHeaders(request);
+    IHttpResponse newResponse = request.execute();
+    if (!(newResponse.getResponse() instanceof HttpResponse)) {
+      return;
+    }
+    copyResponse((HttpResponse) newResponse.getResponse(), (HttpResponse) response.getResponse());
   }
 }
