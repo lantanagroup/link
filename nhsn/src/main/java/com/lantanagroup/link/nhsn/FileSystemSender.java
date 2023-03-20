9a434d77bc4c2264793bc155707ada495c3bb9bd
Steven Williams <steven.williams@lantanagroup.com>
2023-03-20 11:36:12 -0400

    Fix logger class

--- nhsn/src/main/java/com/lantanagroup/link/nhsn/FileSystemSender.java
+++ nhsn/src/main/java/com/lantanagroup/link/nhsn/FileSystemSender.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 @Component
 public class FileSystemSender extends GenericSender implements IReportSender {
-  protected static Logger logger = LoggerFactory.getLogger(AzureBlobStorageSender.class);
+  protected static Logger logger = LoggerFactory.getLogger(FileSystemSender.class);
 
   @Autowired
   @Setter
