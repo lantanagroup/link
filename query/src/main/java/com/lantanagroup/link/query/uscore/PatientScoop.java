90fe598ffc1d889cfaa2b9c7a3260187904c866a
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2023-04-07 08:44:16 -0700

    Updating call to event service to pass the tenant service

--- query/src/main/java/com/lantanagroup/link/query/uscore/PatientScoop.java
+++ query/src/main/java/com/lantanagroup/link/query/uscore/PatientScoop.java
@@ -181,7 +181,7 @@ public class PatientScoop {
 
         // store the data
         try {
-          eventService.triggerDataEvent(EventTypes.AfterPatientDataQuery, patientBundle, criteria, context, null);
+          eventService.triggerDataEvent(tenantService, EventTypes.AfterPatientDataQuery, patientBundle, criteria, context, null);
 
           List<com.lantanagroup.link.db.model.PatientData> dbPatientData = patientData.getBundle().getEntry().stream().map(entry -> {
             com.lantanagroup.link.db.model.PatientData dbpd = new com.lantanagroup.link.db.model.PatientData();
