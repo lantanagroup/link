c7d4c0dcb90057423cf250a601e2929ef0a6402d
seanmcilvenna <sean.mcilvenna@lantanagroup.com>
2023-05-23 14:35:00 -0700

    Adding nhsn-measures.tgz to the validation performance unit test

--- core/src/main/java/com/lantanagroup/link/db/model/tenant/Validation.java
+++ core/src/main/java/com/lantanagroup/link/db/model/tenant/Validation.java
@@ -3,10 +3,11 @@ package com.lantanagroup.link.db.model.tenant;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Validation {
-  private List<String> npmPackages = List.of("uscore.tgz", "qicore.tgz", "deqm.tgz");
+  private List<String> npmPackages = new ArrayList<>(List.of("uscore.tgz", "qicore.tgz", "deqm.tgz"));
 }
