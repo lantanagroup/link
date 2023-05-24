32e91ad7558041cae42f5a613a649524710bd880
seanmcilvenna <sean.mcilvenna@lantanagroup.com>
2023-05-23 17:26:40 -0700

    Refactoring authentication to support either oauth or basic authentication via the "users" collection

--- core/src/main/java/com/lantanagroup/link/db/model/User.java
+++ core/src/main/java/com/lantanagroup/link/db/model/User.java
@@ -2,6 +2,7 @@ package com.lantanagroup.link.db.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 import org.bson.types.ObjectId;
 
 @Getter
@@ -10,4 +11,13 @@ public class User {
   private String id = (new ObjectId()).toString();
   private String name;
   private String email;
+  private String password;
+
+  public boolean hasEmail() {
+    return StringUtils.isNotEmpty(this.email);
+  }
+
+  public boolean hasPassword() {
+    return StringUtils.isNotEmpty(this.password);
+  }
 }
