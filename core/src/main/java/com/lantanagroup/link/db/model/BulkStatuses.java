4a25bf2c3d14403c4285cf6a47e99067d99dc594
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2023-06-26 12:29:41 -0500

    Add extra logging

--- core/src/main/java/com/lantanagroup/link/db/model/BulkStatuses.java
+++ core/src/main/java/com/lantanagroup/link/db/model/BulkStatuses.java
@@ -4,5 +4,6 @@ public class BulkStatuses {
   public static final String pending = "Pending";
   public static final String inProgress = "InProgress";
   public static final String complete = "Complete";
+  public static final String cancelled = "Cancelled";
 //  Pending, InProgress, Complete
 }
