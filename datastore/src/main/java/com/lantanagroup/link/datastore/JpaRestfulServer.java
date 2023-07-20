ae80fca8d4394265b2b3aa3596dd279210453615
Austin Moody <austin.moody@gmail.com>
2023-07-20 12:06:24 -0400

    Data Store now will load FHIR resources (from resources/fhir) as the server starts.
    That directory now has the 7 SearchParameters necessary for it to function.  Any changes to a running Data Store for things like this ultimately need to end up in this directory for future builds.

--- datastore/src/main/java/com/lantanagroup/link/datastore/JpaRestfulServer.java
+++ datastore/src/main/java/com/lantanagroup/link/datastore/JpaRestfulServer.java
@@ -7,11 +7,10 @@ import ca.uhn.fhir.jpa.api.dao.IFhirSystemDao;
 import ca.uhn.fhir.jpa.cache.ResourceChangeListenerCacheFactory;
 import ca.uhn.fhir.jpa.cache.ResourceChangeListenerRegistryImpl;
 import ca.uhn.fhir.jpa.config.HibernatePropertiesProvider;
-import ca.uhn.fhir.jpa.model.entity.ModelConfig;
 import ca.uhn.fhir.jpa.provider.IJpaSystemProvider;
 import ca.uhn.fhir.jpa.provider.JpaCapabilityStatementProvider;
 import ca.uhn.fhir.jpa.searchparam.matcher.InMemoryResourceMatcher;
-import ca.uhn.fhir.jpa.searchparam.registry.SearchParamRegistryImpl;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.server.HardcodedServerAddressStrategy;
 import ca.uhn.fhir.rest.server.RestfulServer;
 import ca.uhn.fhir.rest.server.interceptor.LoggingInterceptor;
@@ -25,21 +24,30 @@ import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.jdbc.dialect.internal.StandardDialectResolver;
 import org.hibernate.engine.jdbc.dialect.spi.DatabaseMetaDataDialectResolutionInfoAdapter;
 import org.hibernate.internal.util.config.ConfigurationException;
+import org.hl7.fhir.r4.model.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.core.io.Resource;
 
 import javax.servlet.ServletException;
 import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serial;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class JpaRestfulServer extends RestfulServer {
+  @Serial
   private static final long serialVersionUID = 1L;
 
   @Autowired
@@ -65,17 +73,18 @@ public class JpaRestfulServer extends RestfulServer {
   private DataSource dataSource;
 
   @Autowired
-  private ModelConfig modelConfig;
-
-  @Autowired
   private DataStoreConfig dataStoreConfig;
 
   @Autowired
   ResourceProviderFactory resourceProviders;
 
+  @Value("classpath:fhir/*")
+  private Resource[] fhirResources;
+
   private FhirContext fhirContext;
-  private ResourceChangeListenerRegistryImpl resourceChangeListenerRegistry = new ResourceChangeListenerRegistryImpl();
-  private InMemoryResourceMatcher inMemoryResourceMatcher = new InMemoryResourceMatcher();
+  private final ResourceChangeListenerRegistryImpl resourceChangeListenerRegistry = new ResourceChangeListenerRegistryImpl();
+
+  private static final Logger logger = LoggerFactory.getLogger(JpaRestfulServer.class);
 
   public JpaRestfulServer() {
   }
@@ -117,12 +126,25 @@ public class JpaRestfulServer extends RestfulServer {
     loggingInterceptor.setLogExceptions(true);
     this.registerInterceptor(loggingInterceptor);
 
+    /*
+    Bundle transactionBundle = new Bundle();
+    transactionBundle.setType(Bundle.BundleType.TRANSACTION);
+    Patient patient = new Patient();
+    patient.addName().setFamily("Smith").addGiven("John");
+    transactionBundle.addEntry().setResource(patient).getRequest().setMethod(Bundle.HTTPVerb.POST);
+
+    Object resultBundle = fhirSystemDao.transaction(null, transactionBundle);
+
+     */
+
+    LoadFhirResources();
+
     this.registerInterceptor(new UserInterceptor(this.dataStoreConfig));
   }
 
   @Primary
   @Bean
-  public HibernatePropertiesProvider jpaStarterDialectProvider(LocalContainerEntityManagerFactoryBean myEntityManagerFactory) throws SQLException {
+  public HibernatePropertiesProvider jpaStarterDialectProvider() {
     try(Connection conn = this.dataSource.getConnection()) {
       DatabaseMetaData metaData = conn.getMetaData();
       return new HibernatePropertiesProvider() {
@@ -133,6 +155,49 @@ public class JpaRestfulServer extends RestfulServer {
       };
     } catch (SQLException sqlException) {
       throw new ConfigurationException(sqlException.getMessage(), sqlException);
+    }
+  }
+
+  private  org.hl7.fhir.r4.model.Resource readFileAsFhirResource(IParser parser, InputStream file) {
+    String resourceString = new BufferedReader(new InputStreamReader(file)).lines().parallel().collect(Collectors.joining("\n"));
+    return (org.hl7.fhir.r4.model.Resource)parser.parseResource(resourceString);
+  }
+
+  private void LoadFhirResources() {
+
+    logger.info("Loading {} FHIR Resources.", fhirResources.length);
+
+    try {
+      IParser parser = fhirContext.newJsonParser();
+      for (final Resource fhirResource : fhirResources) {
+        try (InputStream inputStream = fhirResource.getInputStream()) {
+
+          org.hl7.fhir.r4.model.Resource resource = readFileAsFhirResource(parser, inputStream);
+
+          Bundle transactionBundle = new Bundle();
+          transactionBundle.setType(Bundle.BundleType.TRANSACTION);
+          transactionBundle
+                  .addEntry()
+                  .setResource(resource)
+                  .getRequest()
+                  .setMethod(Bundle.HTTPVerb.PUT)
+                  .setUrl(resource.getResourceType().name() + "/" + resource.getIdElement().getIdPart());
+
+          logger.info("Resource Type '{}' with ID '{}' read from '{}'",
+                  resource.getResourceType().name(),
+                  resource.getIdElement().getIdPart(),
+                  fhirResource.getFilename());
+
+          Bundle resultBundle = (Bundle)fhirSystemDao.transaction(null, transactionBundle);
+
+          logger.info("Resource Type '{}' with ID '{}' PUT with status {}",
+                  resource.getResourceType().name(),
+                  resource.getIdElement().getIdPart(),
+                  resultBundle.getEntry().get(0).getResponse().getStatus());
+        }
+      }
+    } catch (Exception ex) {
+      logger.error(ex.getMessage());
     }
   }
 }
