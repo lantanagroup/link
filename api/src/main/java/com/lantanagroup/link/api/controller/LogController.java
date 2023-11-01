6b89520dd61427dcca7265aeb004aa07e1b22882
seanmcilvenna <sean.mcilvenna@lantanagroup.com>
2023-11-01 14:24:11 -0700

    Refactored log searching to support paging Updated swagger spec to include log searching operation

--- api/src/main/java/com/lantanagroup/link/api/controller/LogController.java
+++ api/src/main/java/com/lantanagroup/link/api/controller/LogController.java
@@ -2,21 +2,32 @@ package com.lantanagroup.link.api.controller;
 
 
 import com.lantanagroup.link.model.LogMessage;
+import com.lantanagroup.link.model.LogSearchResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/log")
 public class LogController extends BaseController {
+  /**
+   * @param startDate The minimum/start date to return logs for
+   * @param endDate   The maximum/end date to return logs for
+   * @param severity  One or more severities to filter the results by
+   * @param page      The page of logs to return
+   * @param content   What content to look for in the message
+   * @return
+   */
   @GetMapping
-  public List<LogMessage> getLogs(String startDate, String endDate, String[] severity) {
+  public LogSearchResponse getLogs(@RequestParam(required = false) String startDate, @RequestParam(required = false) String endDate, @RequestParam(required = false) String[] severity, @RequestParam(required = false, defaultValue = "1") int page, @RequestParam(required = false) String content) {
     // TODO: Replace with actual implementation
-    return List.of(
-            new LogMessage(new Date(), "ERROR", "This is a test message #1", "com.lantanagroup.link.api.ApiInit"),
-            new LogMessage(new Date(), "INFO", "This is a test message #2", "com.lantanagroup.link.api.controller.ApiController"));
+    LogSearchResponse res = new LogSearchResponse();
+    res.setTotal(2);
+    res.getMessages().add(new LogMessage(new Date(), "ERROR", "This is a test message #1", "com.lantanagroup.link.api.ApiInit"));
+    res.getMessages().add(new LogMessage(new Date(), "INFO", "This is a test message #2", "com.lantanagroup.link.api.controller.ApiController"));
+    return res;
   }
 }
