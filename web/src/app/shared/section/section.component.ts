67e9cda883edfcfc821c1eaf255acd92e84fa084
szydlofj <caleb.szydlo@fjorgedigital.com>
2023-12-13 11:39:30 -0600

    adding section component to handle spacing better

---
+++ web/src/app/shared/section/section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.scss']
+})
+export class SectionComponent {
+
+}
