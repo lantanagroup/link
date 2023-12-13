7cf1449a4b329efc390812eaecddabc79258cfe5
szydlofj <caleb.szydlo@fjorgedigital.com>
2023-12-13 16:21:55 -0600

    adding in chart library and setting up dummy data

--- web/src/app/shared/metric/metric.component.ts
+++ web/src/app/shared/metric/metric.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MiniChartComponent } from 'src/app/shared/mini-chart/mini-chart.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp, faArrowDown, faThList } from '@fortawesome/free-solid-svg-icons';
 import { faUp, faDown } from '@fortawesome/pro-solid-svg-icons';
+import { ChartDatapoint, ChartDataModel } from '../interfaces/chart.model';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-metric',
   standalone: true,
-  imports: [CommonModule, MiniChartComponent, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, NgxChartsModule],
   templateUrl: './metric.component.html',
   styleUrls: ['./metric.component.scss']
 })
@@ -18,8 +20,33 @@ export class MetricComponent {
   @Input() changeValue: string = ''; // The rate of value changed
   @Input() changeWindow?: string = 'month'; // the duration of value changed
   @Input() isValueUp: boolean = true; // This will change the path of image
-  @ViewChild(MiniChartComponent) chartComponent!: MiniChartComponent;
+  @Input() data: ChartDatapoint[] = [];
 
   iconUp = faUp ? faUp : faArrowUp
   iconDown = faDown ? faDown : faArrowDown
+
+  // chart render
+
+  // options
+  view: [number, number] = [100, 60];
+  xAxisLabel: string = 'X';
+  yAxisLabel: string = 'Y';
+  colorScheme: {} = {
+    domain: ['#497d0c', '#497d0c']
+  };
+
+  miniChartData: ChartDataModel[] = [];
+
+  ngOnInit() {
+    this.miniChartData = this.setUpChartData()
+  }
+
+  setUpChartData = () => {
+    const chartData = [{
+      name: 'chart data',
+      series: this.data
+    }]
+
+    return chartData
+  }
 }
