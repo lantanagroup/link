959f36ce0e8826f1b7d0a9415fe74a55a12fa530
seanmcilvenna <sean.mcilvenna@lantanagroup.com>
2023-12-14 15:55:13 -0800

    Using latest hapi libraries Changing validator to use HAPI validation libraries, instead of core FHIR validation libraries. All java docs seem to point to that being a better idea. Additionally, there is native support for concurrency in the hapi validation libraries, which is enabled (turned on) in our use of hapi's validation libraries in this commit.

--- core/src/main/java/com/lantanagroup/link/validation/Validator.java
+++ core/src/main/java/com/lantanagroup/link/validation/Validator.java
@@ -2,51 +2,41 @@ package com.lantanagroup.link.validation;
 
 import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.parser.IParser;
-import ca.uhn.fhir.validation.IValidationContext;
-import ca.uhn.fhir.validation.ValidationContext;
-import ca.uhn.fhir.validation.ValidationOptions;
+import ca.uhn.fhir.validation.*;
+import com.lantanagroup.link.Constants;
 import com.lantanagroup.link.FhirContextProvider;
 import com.lantanagroup.link.db.SharedService;
 import lombok.Setter;
-import org.apache.commons.codec.Charsets;
-import org.apache.commons.io.input.ReaderInputStream;
 import org.apache.commons.lang3.StringUtils;
-import org.hl7.fhir.common.hapi.validation.support.*;
+import org.hl7.fhir.common.hapi.validation.support.CachingValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.InMemoryTerminologyServerValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.PrePopulatedValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
-import org.hl7.fhir.common.hapi.validation.validator.VersionSpecificWorkerContextWrapper;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.*;
-import org.hl7.fhir.r5.elementmodel.Manager;
-import org.hl7.fhir.r5.utils.FHIRPathEngine;
-import org.hl7.fhir.r5.utils.XVerExtensionManager;
-import org.hl7.fhir.utilities.validation.ValidationMessage;
-import org.hl7.fhir.validation.instance.InstanceValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Validator {
   protected static final Logger logger = LoggerFactory.getLogger(Validator.class);
 
-  private InstanceValidator instanceValidator;
-  private VersionSpecificWorkerContextWrapper workerContext;
-  private DefaultProfileValidationSupport defaultProfileValidationSupport;
-  private CommonCodeSystemsTerminologyService commonCodeSystemsTerminologyService;
-  private InMemoryTerminologyServerValidationSupport inMemoryTerminologyServerValidationSupport;
-  private PrePopulatedValidationSupport prePopulatedValidationSupport;
+  private final PrePopulatedValidationSupport prePopulatedValidationSupport;
+
+  private FhirValidator validator;
 
   private final IParser jsonParser = FhirContextProvider.getFhirContext().newJsonParser().setPrettyPrint(true);
   private final IParser xmlParser = FhirContextProvider.getFhirContext().newXmlParser();
-  private final ResourceFetcher resourceFetcher;
 
   @Setter
   private SharedService sharedService;
@@ -54,51 +44,84 @@ public class Validator {
   public Validator(SharedService sharedService) {
     this.sharedService = sharedService;
     this.prePopulatedValidationSupport = new PrePopulatedValidationSupport(FhirContextProvider.getFhirContext());
-    this.resourceFetcher = new ResourceFetcher(this.prePopulatedValidationSupport);
+  }
+
+  private static OperationOutcome.IssueSeverity getIssueSeverity(ResultSeverityEnum severity) {
+    switch (severity) {
+      case ERROR:
+        return OperationOutcome.IssueSeverity.ERROR;
+      case WARNING:
+        return OperationOutcome.IssueSeverity.WARNING;
+      case INFORMATION:
+        return OperationOutcome.IssueSeverity.INFORMATION;
+      case FATAL:
+        return OperationOutcome.IssueSeverity.FATAL;
+      default:
+        throw new RuntimeException("Unexpected severity " + severity);
+    }
+  }
+
+  private static OperationOutcome.IssueType getIssueCode(String messageId) {
+    if (messageId == null) {
+      return OperationOutcome.IssueType.NULL;
+    } else if (messageId.startsWith("Rule ")) {
+      return OperationOutcome.IssueType.INVARIANT;
+    }
+
+    switch (messageId) {
+      case "TERMINOLOGY_TX_SYSTEM_NO_CODE":
+        return OperationOutcome.IssueType.INFORMATIONAL;
+      case "Terminology_TX_NoValid_2_CC":
+      case "Terminology_PassThrough_TX_Message":
+      case "Terminology_TX_Code_ValueSet_Ext":
+      case "Terminology_TX_NoValid_17":
+        return OperationOutcome.IssueType.CODEINVALID;
+      case "Extension_EXT_Unknown":
+        return OperationOutcome.IssueType.EXTENSION;
+      case "Measure_MR_M_NotFound":
+        return OperationOutcome.IssueType.NOTFOUND;
+      case "Validation_VAL_Profile_Minimum":
+      case "Validation_VAL_Profile_Maximum":
+      case "Extension_EXT_Type":
+        return OperationOutcome.IssueType.STRUCTURE;
+      case "Type_Specific_Checks_DT_String_WS":
+        return OperationOutcome.IssueType.VALUE;
+      case "Terminology_TX_System_Unknown":
+        return OperationOutcome.IssueType.UNKNOWN;
+      default:
+        return OperationOutcome.IssueType.NULL;
+    }
   }
 
   public void init() {
+    this.loadFiles();
+    this.updateMeasures();
+
+    this.validator = FhirContextProvider.getFhirContext().newValidator();
+    this.validator.setExecutorService(Executors.newWorkStealingPool());
+    IValidatorModule module = new FhirInstanceValidator(this.getCachingValidationSupport());
+    this.validator.registerValidatorModule(module);
+    this.validator.setConcurrentBundleValidation(true);
+  }
+
+  private void loadFiles() {
     try {
-      this.defaultProfileValidationSupport = new DefaultProfileValidationSupport(FhirContextProvider.getFhirContext());
-      this.commonCodeSystemsTerminologyService = new CommonCodeSystemsTerminologyService(FhirContextProvider.getFhirContext());
-      this.inMemoryTerminologyServerValidationSupport = new InMemoryTerminologyServerValidationSupport(FhirContextProvider.getFhirContext());
-
-      this.loadFiles();
-      this.resourceFetcher.refreshCanonicalUrls();
-      this.updateMeasures();
-
-      CachingValidationSupport validationSupport = this.getCachingValidationSupport();
-
-      FHIRPathEngine.IEvaluationContext evaluationCtx = new FhirInstanceValidator.NullEvaluationContext();
-      this.workerContext = VersionSpecificWorkerContextWrapper.newVersionSpecificWorkerContextWrapper(validationSupport);
-      XVerExtensionManager xverManager = new XVerExtensionManager(this.workerContext);
-
-      this.instanceValidator = new InstanceValidator(this.workerContext, evaluationCtx, xverManager);
-      this.instanceValidator.setFetcher(this.resourceFetcher);
-      //this.instanceValidator.setDebug(true);
-      this.instanceValidator.setAnyExtensionsAllowed(true);
-
-      org.hl7.fhir.utilities.validation.ValidationOptions opts =
-              new org.hl7.fhir.utilities.validation.ValidationOptions()
-                      .noCheckValueSetMembership();
-      this.instanceValidator.setBaseOptions(opts);
+      PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+      this.loadClassResources(resolver.getResources("terminology/**"));
+      this.loadClassResources(resolver.getResources("profiles/**"));
     } catch (IOException ex) {
-      logger.error("Error initializing validator", ex);
+      logger.error("Error loading resources for validation", ex);
     }
   }
 
   public void updateMeasures() {
     // Add each measure canonical url to the resource fetcher
     this.sharedService.getMeasureDefinitions().forEach(md -> {
-      md.getBundle().getEntry()
-              .stream()
-              .filter(e -> e.getResource().getResourceType() == ResourceType.Measure)
-              .map(e -> ((Measure) e.getResource()).getUrl())
-              .forEach(this.resourceFetcher::addCanonicalUrl);
+      // TODO
     });
   }
 
-  private void loadClassResources(org.springframework.core.io.Resource[] classResources) throws IOException {
+  private void loadClassResources(org.springframework.core.io.Resource[] classResources) {
     logger.debug("Loading {} class resources for validation", classResources.length);
 
     List<IBaseResource> resources = new ArrayList<>();
@@ -138,62 +161,22 @@ public class Validator {
     }
   }
 
-  private void loadFiles() {
-    try {
-      PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-      this.loadClassResources(resolver.getResources("terminology/**"));
-      this.loadClassResources(resolver.getResources("profiles/**"));
-    } catch (IOException ex) {
-      logger.error("Error loading resources for validation", ex);
-    }
-  }
-
-  private CachingValidationSupport getCachingValidationSupport() throws IOException {
+  private CachingValidationSupport getCachingValidationSupport() {
     ValidationSupportChain validationSupportChain = new ValidationSupportChain(
-            this.defaultProfileValidationSupport,
-            this.inMemoryTerminologyServerValidationSupport,
+            new DefaultProfileValidationSupport(FhirContextProvider.getFhirContext()),
+            new InMemoryTerminologyServerValidationSupport(FhirContextProvider.getFhirContext()),
             this.prePopulatedValidationSupport
     );
     return new CachingValidationSupport(validationSupportChain);
   }
 
-  private static OperationOutcome.IssueSeverity getIssueSeverity(ValidationMessage.IssueSeverity severity) {
-    switch (severity) {
-      case ERROR:
-        return OperationOutcome.IssueSeverity.ERROR;
-      case WARNING:
-        return OperationOutcome.IssueSeverity.WARNING;
-      case INFORMATION:
-        return OperationOutcome.IssueSeverity.INFORMATION;
-      case FATAL:
-        return OperationOutcome.IssueSeverity.FATAL;
-      case NULL:
-        return OperationOutcome.IssueSeverity.NULL;
-      default:
-        throw new RuntimeException("Unexpected severity " + severity);
-    }
-  }
-
-  private static OperationOutcome.IssueType getIssueCode(ValidationMessage.IssueType issueType) {
-    return OperationOutcome.IssueType.fromCode(issueType.toCode());
-  }
-
-  private void validateResource(Resource resource, OperationOutcome outcome, OperationOutcome.IssueSeverity severity, Integer entryIndex) {
+  private void validateResource(Resource resource, OperationOutcome outcome, OperationOutcome.IssueSeverity severity) {
     ValidationOptions opts = new ValidationOptions();
 
-    String resourceJson = this.jsonParser.encodeResourceToString(resource);
-    IValidationContext<IBaseResource> validationContext = ValidationContext.forText(FhirContextProvider.getFhirContext(), resourceJson, opts);
+    ValidationResult result = this.validator.validateWithResult(resource, opts);
 
-    // Have to encode to string here with pretty printing turned on, otherwise the validation messages don't have line/col info
-    String resourceString = validationContext.getResourceAsString();
-    InputStream inputStream = new ReaderInputStream(new StringReader(resourceString), Charsets.UTF_8);
-    Manager.FhirFormat format = Manager.FhirFormat.JSON;
-
-    List<ValidationMessage> messages = new ArrayList<>();
-    this.instanceValidator.validate(null, messages, inputStream, format);
-
-    for (ValidationMessage message : messages) {
-      OperationOutcome.IssueSeverity messageSeverity = getIssueSeverity(message.getLevel());
+    for (SingleValidationMessage message : result.getMessages()) {
+      OperationOutcome.IssueSeverity messageSeverity = getIssueSeverity(message.getSeverity());
 
       // Skip the message depending on the severity filter/arg
       if (severity != null) {
@@ -208,25 +191,19 @@ public class Validator {
         }
       }
 
-      String location = message.getLocation();
+      OperationOutcome.IssueType issueCode = getIssueCode(message.getMessageId());
 
-      if (entryIndex != null) {
-        String ofTypeString = ".ofType(" + resource.getResourceType().toString() + ")";
-
-        if (location.indexOf(".") > 0) {
-          location = "Bundle.entry[" + entryIndex + "].resource" + ofTypeString + "." + location.substring(location.indexOf(".") + 1);
-        } else {
-          location = "Bundle.entry[" + entryIndex + "].resource" + ofTypeString;
-        }
+      if (issueCode == OperationOutcome.IssueType.NULL && message.getMessageId() != null) {
+        logger.warn("Unknown issue code {} for message {}", message.getMessageId(), message.getMessage());
       }
 
       outcome.addIssue()
               .setSeverity(messageSeverity)
-              .setCode(getIssueCode(message.getType()))
+              .setCode(issueCode != null ? issueCode : OperationOutcome.IssueType.UNKNOWN)
               .setDetails(new CodeableConcept().setText(message.getMessage()))
               .setExpression(List.of(
-                      new StringType(location),
-                      new StringType(message.getLine() + ":" + message.getCol())));
+                      new StringType(message.getLocationString()),
+                      new StringType(message.getLocationLine() + ":" + message.getLocationCol())));
     }
   }
 
@@ -239,23 +216,15 @@ public class Validator {
     //noinspection unused
     outcome.setId(UUID.randomUUID().toString());
 
-    if (resource instanceof Bundle) {
-      Bundle bundle = (Bundle) resource;
-      for (int i = 0; i < bundle.getEntry().size(); i++) {
-        Bundle.BundleEntryComponent entry = bundle.getEntry().get(i);
-        this.validateResource(entry.getResource(), outcome, severity, i);
-      }
-    }
-
-    this.validateResource(resource, outcome, severity, null);
+    this.validateResource(resource, outcome, severity);
 
     Date end = new Date();
     logger.debug("Validation took {} seconds", TimeUnit.MILLISECONDS.toSeconds(end.getTime() - start.getTime()));
     logger.debug("Validation found {} issues", outcome.getIssue().size());
 
     // Add extensions (which don't formally exist) that show the total issue count and severity threshold
-    outcome.addExtension("http://nhsnlink.org/oo-total", new IntegerType(outcome.getIssue().size()));
-    outcome.addExtension("http://nhsnlink.org/oo-severity", new CodeType(severity.toCode()));
+    outcome.addExtension(Constants.OperationOutcomeTotalExtensionUrl, new IntegerType(outcome.getIssue().size()));
+    outcome.addExtension(Constants.OperationOutcomeSeverityExtensionUrl, new CodeType(severity.toCode()));
 
     return outcome;
   }
