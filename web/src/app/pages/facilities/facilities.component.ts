92730e56f5f98fda750369149c410b302a7fe499
szydlofj <caleb.szydlo@fjorgedigital.com>
2023-12-15 12:33:14 -0600

    adding facility child pages with linking

--- web/src/app/pages/facilities/facilities.component.ts
+++ web/src/app/pages/facilities/facilities.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from 'src/app/shared/hero/hero.component';
-import { AccordionComponent } from 'src/app/shared/accordion/accordion.component';
 import { IconComponent } from 'src/app/shared/icon/icon.component';
 import { ButtonComponent } from 'src/app/shared/button/button.component';
+import { SectionComponent } from 'src/app/shared/section/section.component';
+import { SectionHeadingComponent } from 'src/app/shared/section-heading/section-heading.component';
 
 @Component({
   selector: 'app-facilities',
   standalone: true,
-  imports: [CommonModule, HeroComponent, IconComponent, ButtonComponent, AccordionComponent],
+  imports: [CommonModule, HeroComponent, IconComponent, ButtonComponent, SectionComponent, SectionHeadingComponent],
   templateUrl: './facilities.component.html',
   styleUrls: ['./facilities.component.css']
 })
