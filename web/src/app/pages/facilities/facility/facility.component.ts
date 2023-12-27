d30d0fe7d379663a75bd37b29da908d0c81c5d74
fjorge-saran <79157822+fjorge-saran@users.noreply.github.com>
2023-12-27 13:24:02 -0600

    - saving changes for facilities profile.

--- web/src/app/pages/facilities/facility/facility.component.ts
+++ web/src/app/pages/facilities/facility/facility.component.ts
@@ -6,33 +6,47 @@ import { SectionComponent } from 'src/app/shared/section/section.component';
 import { SectionHeadingComponent } from 'src/app/shared/section-heading/section-heading.component';
 import { ButtonComponent } from 'src/app/shared/button/button.component';
 import { IconComponent } from 'src/app/shared/icon/icon.component';
+import { FacilitiesApiService } from 'src/services/api/facilities/facilities-api.service';
+import { CardComponent } from "../../../shared/card/card.component";
 
 @Component({
-  selector: 'app-facility',
-  standalone: true,
-  imports: [CommonModule, HeroComponent, SectionComponent, SectionHeadingComponent, ButtonComponent, IconComponent],
-  templateUrl: './facility.component.html',
-  styleUrls: ['./facility.component.scss']
+    selector: 'app-facility',
+    standalone: true,
+    templateUrl: './facility.component.html',
+    styleUrls: ['./facility.component.scss'],
+    imports: [CommonModule, HeroComponent, SectionComponent, SectionHeadingComponent, ButtonComponent, IconComponent, CardComponent]
 })
 export class FacilityComponent {
   facilityId: string | null = null
+  facilityDetails: any = null;
 
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private facilitiesApiService: FacilitiesApiService
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.facilityId = params.get('id')
 
       if(this.facilityId) {
         // todo : make API call
+        this.GetFacilityDetails(this.facilityId);
       } else {
         this.router.navigate(['/facilities'])
       }
     })
+  }
+
+  async GetFacilityDetails(id: string) {
+    try {
+      const tenantDetail = await this.facilitiesApiService.fetchFacilityById(id);
+      this.facilityDetails = tenantDetail;
+    } catch (error) {
+      console.error('Error Loading table data.', error);
+    }
   }
 
   generateEditLink(): { url: string } {
