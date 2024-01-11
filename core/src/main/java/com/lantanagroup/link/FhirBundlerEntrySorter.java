abbe1cb33197bb7a057a39e06e3e27c160b79c83
seanmcilvenna <sean.mcilvenna@lantanagroup.com>
2024-01-11 11:05:31 -0800

    Missed an import

--- core/src/main/java/com/lantanagroup/link/FhirBundlerEntrySorter.java
+++ core/src/main/java/com/lantanagroup/link/FhirBundlerEntrySorter.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FhirBundlerEntrySorter {
   private static final Logger logger = LoggerFactory.getLogger(FhirBundlerEntrySorter.class);
