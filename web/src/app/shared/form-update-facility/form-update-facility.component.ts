b3f273376a5fe7ef54521cea2bb8546d65c66f6a
szydlofj <caleb.szydlo@fjorgedigital.com>
2024-01-19 10:32:31 -0600

    updating form config

--- web/src/app/shared/form-update-facility/form-update-facility.component.ts
+++ web/src/app/shared/form-update-facility/form-update-facility.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormArray, FormBuilder, Validators, Form } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import * as yaml from 'js-yaml'
 import { SectionHeadingComponent } from '../section-heading/section-heading.component';
 import { AccordionComponent } from '../accordion/accordion.component';
 import { ButtonComponent } from '../button/button.component';
@@ -10,6 +11,22 @@ import { ToastComponent } from '../toast/toast.component';
 import { ToastService } from 'src/services/toasts/toast.service';
 import { Router } from '@angular/router';
 import { FacilitiesApiService } from 'src/services/api/facilities/facilities-api.service';
+import { GlobalApiService } from 'src/services/api/globals/globals-api.service';
+import { Events, NormalizationClass, QueryPlans, TenantConceptMap, TenantDetails, Schedule } from '../interfaces/tenant.model';
+
+interface FormSchedule {
+  cron: string
+  measureIds: string
+  regenerateIfExists: number
+  reportingPeriodMethod: "LastMonth" | "LastWeek" | "CurrentMonth" | "CurrentWeek"
+}
+
+interface FormConceptMap {
+  id: string
+  name?: string
+  contexts: string
+  map?: any // todo: not sure what this looks like 
+}
 
 @Component({
   selector: 'app-form-update-facility',
@@ -22,8 +39,14 @@ export class FormUpdateFacilityComponent {
 
   @Input() facilityId?: string | null = null;
 
-  constructor(private fb: FormBuilder, private facilitiesApiService: FacilitiesApiService, private toastService: ToastService, private router: Router) { }
-  facilityDetails: any = null;
+  constructor(
+    private fb: FormBuilder, 
+    private facilitiesApiService: FacilitiesApiService, 
+    private globalApiService: GlobalApiService,
+    private toastService: ToastService, 
+    private router: Router
+  ) { }
+  facilityDetails: TenantDetails | null = null;
   isDataLoaded: boolean = false;
 
   async GetFacilityDetails(id: string) {
@@ -36,50 +59,45 @@ export class FormUpdateFacilityComponent {
     }
   }
 
-  facilitiesForm = new FormGroup({
-    profile: new FormGroup({
-      tenantId: new FormControl('', [Validators.required]),
-      name: new FormControl('', [Validators.required]),
-      description: new FormControl(''),
-      bundleName: new FormControl('', [Validators.required]),
-      cdcOrgId: new FormControl(''),
-      database: new FormControl(''),
-      vendor: new FormControl(''),
-      dataRetentionPeriod: new FormControl('')
+  facilitiesForm = new FormGroup ({
+    id: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+    bundling: new FormGroup ({
+      name: new FormControl('', [Validators.required])
     }),
-    normalizations: new FormGroup({
-      codeSystemCleanup: new FormControl(0),
-      containedResourceCleanup: new FormControl(0),
-      copyLocation: new FormControl(0),
-      encounterStatus: new FormControl(0),
-      fixPeriodDates: new FormControl(0),
-      fixResourceIds: new FormControl(0),
-      patientDataResource: new FormControl(0)
+    cdcOrgId: new FormControl(''),
+    connectionString: new FormControl(''),
+    vendor: new FormControl(''),
+    retentionPeriod: new FormControl(''),
+    events: new FormGroup ({
+      afterPatientDataQuery: new FormGroup({
+        codeSystemCleanup: new FormControl(0),
+        containedResourceCleanup: new FormControl(0),
+        copyLocation: new FormControl(0),
+        encounterStatus: new FormControl(0),
+        fixPeriodDates: new FormControl(0),
+        fixResourceIds: new FormControl(0),
+      }),
+      afterPatientResourceQuery: new FormGroup({
+        patientDataResource: new FormControl(0)
+      })
     }),
     conceptMaps: this.fb.array([]),
     scheduling: new FormGroup({
-      queryPatientList: new FormControl(''),
-      dataRetentionCheck: new FormControl(''),
-      schedules: this.fb.array([], [Validators.required])
+      queryPatientListCron: new FormControl(''),
+      dataRetentionCheckCron: new FormControl(''),
+      generateAndSubmitReports: this.fb.array([], [Validators.required])
     }),
-    nativeFHIRQuery: new FormGroup({
-      nativeFHIREndpoint: new FormControl(''),
-      parallelPatients: new FormControl(),
-      authenticationMethod: new FormControl('None')
+    fhirQuery: new FormGroup({
+      fhirServerBase: new FormControl(''),
+      parallelPatients: new FormControl(''),
+      authClass: new FormControl(''),
+      queryPlans: this.fb.array([])
     }),
-    censusAcquisition: new FormGroup({
-      method: new FormControl('')
-    }),
-    bulkFHIR: new FormGroup({
-      bulkFHIREndpoint: new FormControl(''),
-      waitInterval: new FormControl(),
-      groupID: new FormControl(''),
-      initialResponseURLHeader: new FormControl(''),
-      progressHeaderName: new FormControl(''),
-      progressCompleteHeaderValue: new FormControl('')
-    }),
-    queryPlans: this.fb.array([])
-  });
+    censusAcquisitionMethod: new FormControl(''),
+    bulkWaitTimeInMilliseconds: new FormControl('')
+  })
 
   // Concept Maps Dynamic Fields
   get conceptMaps(): FormArray {
@@ -113,15 +131,15 @@ export class FormUpdateFacilityComponent {
 
   // Schedules Dynamic Fields
   get schedules(): FormArray {
-    return this.facilitiesForm.get('scheduling.schedules') as FormArray;
+    return this.facilitiesForm.get('scheduling.generateAndSubmitReports') as FormArray;
   }
 
   createSchedule(): FormGroup {
     return this.fb.group({
       measureIds: new FormControl(''),
-      reportingPeriod: new FormControl('Last Month'),
-      schedule: new FormControl(''),
-      regenerate: new FormControl(1)
+      reportingPeriodMethod: new FormControl('LastMonth'),
+      cron: new FormControl(''),
+      regenerateIfExists: new FormControl(1)
     });
   }
 
@@ -143,7 +161,7 @@ export class FormUpdateFacilityComponent {
 
   // Query Plan Dynamic Fields
   get queryPlans(): FormArray {
-    return this.facilitiesForm.get('queryPlans') as FormArray;
+    return this.facilitiesForm.get('fhirQuery.queryPlans') as FormArray;
   }
 
   createQueryPlan(): FormGroup {
@@ -173,7 +191,8 @@ export class FormUpdateFacilityComponent {
 
   async ngOnInit(): Promise<void> {
     if (this.facilityId) {
-      await this.setInitialValues(this.facilityId);
+      await this.GetFacilityDetails(this.facilityId)
+      await this.setInitialValues(this.facilityId)
     } else {
       this.isDataLoaded = true;
     }
@@ -181,121 +200,250 @@ export class FormUpdateFacilityComponent {
 
 
   private async setInitialValues(facilityId: string) {
-    const tenantDetail = await this.facilitiesApiService.fetchFacilityById(facilityId);
+    const conceptMaps = await this.globalApiService.getContent(`${facilityId}/conceptMap`)
+
+    console.log('facility details:', this.facilityDetails)
+    console.log('Concept Maps:', conceptMaps)
 
     // Schedules
-    if (tenantDetail?.scheduling?.generateAndSubmitReports) {
-      for (let i = 0; i < tenantDetail.scheduling.generateAndSubmitReports.length; i++) {
+    if (this.facilityDetails?.scheduling?.generateAndSubmitReports) {
+      for (const schedule of this.facilityDetails.scheduling.generateAndSubmitReports) {
         this.addSchedule();
       }
     }
 
     // Concept Maps
-    if (tenantDetail?.conceptMaps) {
-      for (const cm of tenantDetail.conceptMaps) {
+    if (conceptMaps) {
+      for (const cm of conceptMaps) {
         this.addConceptMap();
       }
     }
 
     // Query Plans
-    if (tenantDetail?.queryPlans) {
-      for (const qp of tenantDetail.queryPlans) {
+    if (this.facilityDetails?.fhirQuery?.queryPlans) {
+      const queryPlans = this.facilityDetails.fhirQuery.queryPlans
+      let count = 0;
+      if(typeof queryPlans === 'object' && queryPlans !== null && !Array.isArray(queryPlans)) {
+        count = Object.keys(queryPlans).length
+      } else {
+        count = queryPlans.length
+      }
+      for (let i = 0; i < count; i++) {
         this.addQueryPlan();
       }
     }
 
     this.facilitiesForm.patchValue({
 
-      profile: {
-        tenantId: this.facilityId,
-        name: tenantDetail?.name,
-        description: 'This is placeholder content that we assign because there is an ID passed into this form', // TODO:CheckClient
-        bundleName: tenantDetail?.bundling?.name,
-        cdcOrgId: tenantDetail?.cdcOrgId,
-        database: tenantDetail?.connectionString,
-        vendor: tenantDetail?.vendor, // TODO:CheckClient
-        dataRetentionPeriod: tenantDetail?.retentionPeriod,
+      id: this.facilityId,
+      name: this.facilityDetails?.name,
+      description: null, // todo : doesn't exist in endpoint
+      bundling: {
+        name: this.facilityDetails?.bundling?.name
       },
-      normalizations: {
-        codeSystemCleanup: tenantDetail?.normalizations?.codeSystemCleanup,
-        containedResourceCleanup: tenantDetail?.normalizations?.containedResourceCleanup,
-        copyLocation: tenantDetail?.normalizations?.copyLocation,
-        encounterStatus: tenantDetail?.normalizations?.encounterStatus,
-        fixPeriodDates: tenantDetail?.normalizations?.fixPeriodDates,
-        fixResourceIds: tenantDetail?.normalizations?.fixResourceIds,
-        patientDataResource: tenantDetail?.normalizations?.patientDataResource
+      cdcOrgId: this.facilityDetails?.cdcOrgId,
+      connectionString: this.facilityDetails?.connectionString,
+      vendor: null, // todo : doesn't exist in endpoint
+      retentionPeriod: this.facilityDetails?.retentionPeriod,
+      events: this.mapNormalizationsToFormData(this.facilityDetails?.events),
+      conceptMaps: this.mapConceptMapData(conceptMaps),
+      scheduling: {
+        queryPatientListCron: this.facilityDetails?.scheduling?.queryPatientListCron,
+        dataRetentionCheckCron: this.facilityDetails?.scheduling?.dataRetentionCheckCron,
+        generateAndSubmitReports: this.mapSchedulingData(this.facilityDetails?.scheduling?.generateAndSubmitReports)
       },
-      conceptMaps: tenantDetail?.conceptMaps || [],
-      scheduling: this.mapSchedulingApiDataToFormData(tenantDetail?.scheduling),
-      nativeFHIRQuery: {
-        // nativeFHIREndpoint: tenantDetail.fhirQuery.fhirServerBase, // TODO:
-        // parallelPatients: tenantDetail.fhirQuery.parallelPatients, // TODO:
-        // authenticationMethod: tenantDetail.fhirQuery.authClass // TODO:
+      fhirQuery: {
+        fhirServerBase: this.facilityDetails?.fhirQuery?.fhirServerBase,
+        parallelPatients: this.facilityDetails?.fhirQuery?.parallelPatients.toString(),
+        authClass: this.facilityDetails?.fhirQuery?.authClass,
+        queryPlans: this.mapQueryPlansApiDataToFormData(this.facilityDetails?.fhirQuery?.queryPlans)
       },
-      censusAcquisition: {
-        method: tenantDetail?.censusAcquisition?.method
-      },
-      bulkFHIR: {
-        bulkFHIREndpoint: tenantDetail?.bulkFHIR?.bulkFHIREndpoint,
-        waitInterval: tenantDetail?.bulkFHIR?.waitInterval,
-        groupID: tenantDetail?.bulkFHIR?.groupID,
-        initialResponseURLHeader: tenantDetail?.bulkFHIR?.initialResponseURLHeader,
-        progressHeaderName: tenantDetail?.bulkFHIR?.progressHeaderName,
-        progressCompleteHeaderValue: tenantDetail?.bulkFHIR?.progressCompleteHeaderValue
-      },
-      queryPlans: tenantDetail?.queryPlans || []
+      censusAcquisitionMethod: '', // todo : doesn't exist in endpoint
+      bulkWaitTimeInMilliseconds: this.facilityDetails?.bulkWaitTimeInMilliseconds
     })
 
     this.isDataLoaded = true;
   }
 
-  // handle submit
+  /// ********************* ///
+  /// * MAPPING FUNCTIONS * ///
+  /// ********************* ///
+
+  // * Map Scheduling Data
+
+  mapSchedulingData(data: Schedule[] | FormSchedule[] | undefined) {
+    if (!data) {
+      return []
+    }
+
+    const generateAndSubmitReports = data.map((schedule: Schedule | FormSchedule) => {
+      return {
+        cron: schedule.cron,
+        measureIds: this.convertLineBreaks(schedule.measureIds),
+        regenerateIfExists: schedule.regenerateIfExists ? 1 : 0,
+        reportingPeriodMethod: schedule.reportingPeriodMethod
+      }
+    })
+
+    return generateAndSubmitReports;
+  }
+
+
+  // * Map Concept Maps 
+
+  mapConceptMapData(data: TenantConceptMap[] | FormConceptMap[]) {
+    if(!data) {
+      return []
+    }
+    const conceptMaps = data.map((cm: TenantConceptMap | FormConceptMap) => {
+      return {
+        id: cm?.id,
+        name: cm?.name,
+        contexts: this.convertLineBreaks(cm?.contexts),
+        map: cm?.map
+      }
+    })
+    return conceptMaps
+  }
+
+  // * Map Query Plans
+
+  mapQueryPlansApiDataToFormData(data: QueryPlans | undefined) { 
+    if(!data) {
+      return []
+    }
+    let queryPlans: any = []
+    for (const key of Object.keys(data)) {
+      queryPlans = [
+        ...queryPlans,
+        {
+          measureId: key,
+          plan: this.convertJsonToYaml(data[key])
+        }
+      ]
+    }
+    return queryPlans
+  }
+
+
+  // Map Normalizations
+  mapNormalizationsToFormData(data: Events | undefined) {
+
+    if(!data) {
+      return {}
+    }
+
+    let afterPatientDataQueryEvents = {}
+    let afterPatientResourceQueryEvents = {}
+    if(data?.afterPatientDataQuery !== null) {
+      const thisEvent = data.afterPatientDataQuery
+    
+      afterPatientDataQueryEvents = {
+        codeSystemCleanup: thisEvent.some((item: string) => item === NormalizationClass.CodeSystemCleanup) ? 1 : 0,
+        containedResourceCleanup: thisEvent.some((item: string) => item === NormalizationClass.ContainedResourceCleanup) ? 1 : 0,
+        copyLocation: thisEvent.some((item: string) => item === NormalizationClass.CopyLocationIdentifierToType) ? 1 : 0,
+        encounterStatus: thisEvent.some((item: string) => item === NormalizationClass.EncounterStatusTransformer) ? 1 : 0,
+        fixPeriodDates: thisEvent.some((item: string) => item === NormalizationClass.FixPeriodDates) ? 1 : 0,
+        fixResourceIds: thisEvent.some((item: string) => item === NormalizationClass.FixResourceId) ? 1 : 0
+      }
+    }
+
+    if(data?.afterPatientResourceQuery !== null) {
+      afterPatientDataQueryEvents = {
+        patientDataResource: data.afterPatientResourceQuery.some((item: string) => item === NormalizationClass.PatientDataResourceFilter) ? 1 : 0
+      }
+    }
+
+    const normalizations = {
+      afterPatientDataQuery: afterPatientDataQueryEvents,
+      afterPatientResourceQuery: afterPatientResourceQueryEvents
+    }
+
+    return normalizations
+  }
+
+  /// ******************** ///
+  /// * Helper Functions * ///
+  /// ******************** ///
+
+  // Function to convert line breaks to array and back
+  convertLineBreaks(data: string | string[]) {
+    if( typeof data === 'string' )
+      return data.split('\n')
+    else {
+      return data.join('\n')
+    }
+  }
+
+  // Functions to convert JSON to YAML and back
+  convertJsonToYaml(jsonData: any): string {
+    try {
+      return yaml.dump(jsonData)
+    } catch(error) {
+      console.error('Error converting JSON to YAML', error)
+      return ''
+    }
+  }
+
+  convertYamlToJson(yamlData: any) {
+    try {
+        const jsonObject = yaml.load(yamlData)
+        return JSON.stringify(jsonObject, null, 2) // The second and third arguments beautify the output
+    } catch (error) {
+        console.error('Error converting YAML to JSON', error)
+        return null
+    }
+  }
+
+
+  /// *************** ///
+  /// * Form Submit * ///
+  /// *************** ///
 
   async onSubmit() {
     const formData = this.facilitiesForm.value;
-    const submissionData = {
-      id: formData.profile?.tenantId,
-      name: formData.profile?.name,
-      retentionPeriod: formData.profile?.dataRetentionPeriod,
-      connectionString: formData.profile?.database,
-      cdcOrgId: formData.profile?.cdcOrgId,
+    console.log('formData:', formData)
+    // todo : separate out concept map data
+    if(this.facilitiesForm.value?.conceptMaps) {
+      // todo : fix typescript error
+      const conceptMapData = this.mapConceptMapData([])
+    }
 
-      bundling: {
-        name: formData?.profile?.bundleName
-      }
-      // calculate other elements as well for the request
+    const tenantData = {
+
     };
+
     if (this.facilitiesForm.valid) {
       try {
         let response;
         if (this.facilityId) {
           // Update existing facility
-          response = await this.facilitiesApiService.updateFacility(this.facilityId, submissionData);
+          // response = await this.facilitiesApiService.updateFacility(this.facilityId, submissionData);
           // response = await this.facilitiesApiService.updateFacility(this.facilityId, this.facilitiesForm.value);
 
           this.toastService.showToast(
             'Facility Updated',
-            `${this.facilitiesForm.value.profile?.name} has been successfully updated.`,
+            `${this.facilitiesForm.value.name} has been successfully updated.`,
             'success'
           )
-          this.router.navigate(['/facilities/facility', this.facilityId])
+          // this.router.navigate(['/facilities/facility', this.facilityId])
         } else {
           // Create new facility
-          response = await this.facilitiesApiService.createFacility(submissionData);
+          // response = await this.facilitiesApiService.createFacility(submissionData);
           // response = await this.facilitiesApiService.createFacility(this.facilitiesForm.value);
 
           this.toastService.showToast(
             'Facility Created',
-            `${this.facilitiesForm.value.profile?.name} has been successfully created.`,
+            `${this.facilitiesForm.value.name} has been successfully created.`,
             'success'
           )
-          this.router.navigate(['/facilities/'])
+          // this.router.navigate(['/facilities/'])
         }
       } catch (error) {
         console.error('Error submitting facility data', error);
         this.toastService.showToast(
           'Facility Error',
-          `Error while adding/updating ${this.facilitiesForm.value.profile?.name}.`,
+          `Error while adding/updating ${this.facilitiesForm.value.name}.`,
           'failed'
         )
       }
@@ -303,49 +451,5 @@ export class FormUpdateFacilityComponent {
     } else {
       this.facilitiesForm.markAllAsTouched()
     }
-  }
-
-
-  /// MAPPING FUNCTIONS
-
-  // Map Scheduling Data
-  mapSchedulingApiDataToFormData(schedulingData: any) {
-    if (!schedulingData) {
-      return {
-        queryPatientList: null,
-        dataRetentionCheck: null,
-        schedules: []
-      };
-    }
-
-    const { queryPatientListCron, dataRetentionCheckCron, generateAndSubmitReports } = schedulingData as {
-      queryPatientListCron: string,
-      dataRetentionCheckCron: string,
-      generateAndSubmitReports: {
-        cron: string,
-        measureIds: string[],
-        reportingPeriodMethod: string,
-        regenerateIfExists: boolean
-      }[]
-    };
-
-    // Transforming generateAndSubmitReports to match the form's schedule structure
-    const schedules = generateAndSubmitReports.map(report => {
-      return {
-        measureIds: report.measureIds.join(', '),
-        reportingPeriod: report.reportingPeriodMethod,
-        schedule: report.cron,
-        regenerate: report.regenerateIfExists ? 1 : 0
-      };
-    });
-
-    // Creating the transformed scheduling object for the form
-    const formSchedulingData = {
-      queryPatientList: queryPatientListCron,
-      dataRetentionCheck: dataRetentionCheckCron,
-      schedules: schedules
-    };
-
-    return formSchedulingData;
   }
 }
