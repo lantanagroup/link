a5cc1606d7ee200c27876e27a0aaca53e846ca38
szydlofj <caleb.szydlo@fjorgedigital.com>
2024-01-24 12:38:45 -0600

    changing metric card time format

--- web/src/app/helpers/GlobalPipes.pipe.ts
+++ web/src/app/helpers/GlobalPipes.pipe.ts
@@ -98,7 +98,7 @@ export class MillisecondsToTimestamp implements PipeTransform {
   }
 }
 
-// Truncated, milliseconds to Hours
+// Truncated, milliseconds to Minutes
 @Pipe({
   name: 'millisecondsToDisplay',
   standalone: true
@@ -109,12 +109,15 @@ export class MillisecondsToDisplay implements PipeTransform {
           minutes = Math.floor((actualValue / (1000 * 60)) % 60),
           seconds = Math.floor((actualValue / 1000) % 60)
 
+
+    console.log(`hours:minutes:seconds - ${hours}:${minutes}:${seconds}`)
+
     if(hours > 0) {
-      return {value: hours.toString(), unit: 'hours'}
-    } else if (minutes > 0) {
-      return {value: minutes.toString(), unit: 'minutes'}
+      const remainder = minutes / 60 * 100
+      return {value: `${hours.toString()}.${remainder.toString()}`, unit: 'hours'}
     } else {
-      return {value: seconds.toString(), unit: 'seconds'}
+      const remainder = seconds / 60 * 100
+      return {value: `${minutes.toString()}.${remainder.toString()}`, unit: 'minutes'}
     }
   }
 }
