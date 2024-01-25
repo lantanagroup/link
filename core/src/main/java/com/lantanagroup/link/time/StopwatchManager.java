fc345bc86ef26be23c763762d50cf160d3c7f23d
MontaltoNick <nick.montalto@lantanagroup.com>
2024-01-25 15:07:18 -0600

    Add Category handling to logging output

--- core/src/main/java/com/lantanagroup/link/time/StopwatchManager.java
+++ core/src/main/java/com/lantanagroup/link/time/StopwatchManager.java
@@ -1,21 +1,18 @@
 package com.lantanagroup.link.time;
 
+import com.lantanagroup.link.db.SharedService;
 import com.lantanagroup.link.db.model.MetricData;
 import com.lantanagroup.link.db.model.Metrics;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.*;
 
-import com.lantanagroup.link.db.SharedService;
-
 @Component
 public class StopwatchManager {
   private static final int COUNT_WIDTH = 6;
   private static final int DURATION_WIDTH = 11;
-
   private SharedService sharedService;
 
   private final Map<String, List<Duration>> durationsByTask = new LinkedHashMap<>();
@@ -37,14 +34,24 @@ public class StopwatchManager {
             .mapToInt(String::length)
             .max()
             .orElse(DURATION_WIDTH);
-    Formatter formatter = new Formatter(taskWidth, COUNT_WIDTH, DURATION_WIDTH);
+
+    var maxCategory = durationsByTask.entrySet().stream().max((s,e) -> s.getKey().substring(s.getKey().indexOf(":") + 1).length()).get();
+    var categoryWidth = maxCategory.getKey().substring(maxCategory.getKey().indexOf(":") + 1).length() + 5;
+
+    if(categoryWidth < 15){
+      categoryWidth = 15;
+    }
+
+    Formatter formatter = new Formatter(categoryWidth, taskWidth, COUNT_WIDTH, DURATION_WIDTH);
     StringBuilder statistics = new StringBuilder();
-    statistics.append(formatter.getHeaderLine("Task", "Count", "Total", "Min", "Mean", "Max"));
+    statistics.append(formatter.getHeaderLine("Category","Task", "Count", "Total", "Min", "Mean", "Max"));
     for (Map.Entry<String, List<Duration>> entry : durationsByTask.entrySet()) {
       String key = entry.getKey();
       String task = key.substring(0, key.indexOf(":"));
+      String category = key.substring(key.indexOf(":") + 1);
       List<Duration> durations = entry.getValue();
       statistics.append(formatter.getValueLine(
+            category,
             task,
             durations.size(),
             getTotal(durations),
@@ -105,15 +112,18 @@ public class StopwatchManager {
     private final int taskWidth;
     private final int countWidth;
     private final int durationWidth;
+    private final int categoryWidth;
 
-    public Formatter(int taskWidth, int countWidth, int durationWidth) {
+    public Formatter(int categoryWidth, int taskWidth, int countWidth, int durationWidth) {
+      this.categoryWidth = categoryWidth;
       this.taskWidth = taskWidth;
       this.countWidth = countWidth;
       this.durationWidth = durationWidth;
     }
 
-    public String getHeaderLine(String task, String count, String... durations) {
+    public String getHeaderLine(String category, String task, String count, String... durations) {
       List<String> headers = new ArrayList<>();
+      headers.add(StringUtils.center(category, categoryWidth));
       headers.add(StringUtils.center(task, taskWidth));
       headers.add(StringUtils.center(count, countWidth));
       for (String duration : durations) {
@@ -122,8 +132,9 @@ public class StopwatchManager {
       return String.join(SEPARATOR, headers) + System.lineSeparator();
     }
 
-    public String getValueLine(String task, int count, Duration... durations) {
+    public String getValueLine(String category, String task, int count, Duration... durations) {
       List<String> values = new ArrayList<>();
+      values.add(StringUtils.rightPad(category, categoryWidth));
       values.add(StringUtils.rightPad(task, taskWidth));
       values.add(StringUtils.leftPad(Integer.toString(count), countWidth));
       for (Duration duration : durations) {
