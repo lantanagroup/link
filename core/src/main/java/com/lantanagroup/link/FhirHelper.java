35fa5964fbb5df506e047d5500750051a0524924
Steven Williams <steven.williams@lantanagroup.com>
2024-07-13 16:11:00 -0400

    Remove unused parameter

--- core/src/main/java/com/lantanagroup/link/FhirHelper.java
+++ core/src/main/java/com/lantanagroup/link/FhirHelper.java
@@ -13,7 +13,6 @@ import com.lantanagroup.link.model.ApiVersionModel;
 import com.lantanagroup.link.serialize.FhirJsonDeserializer;
 import com.lantanagroup.link.serialize.FhirJsonSerializer;
 import com.lantanagroup.link.validation.ClasspathBasedValidationSupport;
-import com.lantanagroup.link.validation.Validator;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
@@ -285,7 +284,7 @@ public class FhirHelper {
     return device;
   }
 
-  public static Device getDevice(ApiConfig apiConfig, TenantService tenantService, Validator validator) {
+  public static Device getDevice(ApiConfig apiConfig, TenantService tenantService) {
     Device device = getDevice(apiConfig);
 
     if (tenantService.getConfig().getEvents() != null) {
@@ -324,12 +323,12 @@ public class FhirHelper {
       }
     }
 
-    setImplementationGuideNotes(device, validator);
+    setImplementationGuideNotes(device);
 
     return device;
   }
 
-  public static void setImplementationGuideNotes(Device device, Validator validator) {
+  public static void setImplementationGuideNotes(Device device) {
     String prefix = "Implementation Guide: ";
     device.getNote().removeIf(annotation -> annotation.getText().startsWith(prefix));
 
