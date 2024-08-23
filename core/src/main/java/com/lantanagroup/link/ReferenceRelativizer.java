565fe813cbc5900c07b9ecb8781a073dd6cc6f8b
Steven Williams <steven.williams@lantanagroup.com>
2024-08-23 16:37:38 -0400

    Find references in bundle entries

--- core/src/main/java/com/lantanagroup/link/ReferenceRelativizer.java
+++ core/src/main/java/com/lantanagroup/link/ReferenceRelativizer.java
@@ -1,19 +1,39 @@
 package com.lantanagroup.link;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.util.FhirTerser;
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.StringType;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ReferenceRelativizer {
   private final FhirContext fhirContext;
 
   public ReferenceRelativizer(FhirContext fhirContext) {
     this.fhirContext = fhirContext;
+  }
+
+  private List<Reference> getReferences(IBaseResource resource) {
+    List<IBaseResource> resources;
+    if (resource instanceof Bundle) {
+      resources = ((Bundle) resource).getEntry().stream()
+              .map(Bundle.BundleEntryComponent::getResource)
+              .filter(Objects::nonNull)
+              .collect(Collectors.toList());
+    } else {
+      resources = List.of(resource);
+    }
+    FhirTerser fhirTerser = fhirContext.newTerser();
+    return resources.stream()
+            .flatMap(_resource -> fhirTerser.getAllPopulatedChildElementsOfType(_resource, Reference.class).stream())
+            .collect(Collectors.toList());
   }
 
   /**
@@ -23,8 +43,7 @@ public class ReferenceRelativizer {
    */
   public void relativize(IBaseResource resource, String expectedBaseUrl) {
     String cleanExpectedBaseUrl = expectedBaseUrl == null ? null : StringUtils.stripEnd(expectedBaseUrl, "/");
-    List<Reference> references = fhirContext.newTerser().getAllPopulatedChildElementsOfType(resource, Reference.class);
-    for (Reference reference : references) {
+    for (Reference reference : getReferences(resource)) {
       String referenceElement = reference.getReference();
       if (StringUtils.isEmpty(referenceElement)) {
         continue;
